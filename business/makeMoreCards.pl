886871459d9e0af2ec103eb3c9994717c4868e42
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2009-11-20 07:00:07 +0000

    From cards.yaml, 2,4-partner discussion cards, quiz cards, HTML quiz

---
+++ business/makeMoreCards.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use YAML qw/LoadFile DumpFile/;
+use IO::All;
+use Text::Template;
+
+my $cards = LoadFile "$ARGV[0]/cards.yaml";
+
+for my $t ( keys %$cards ) {
+	my $topic = $cards->{$t};
+	next unless ref $topic eq 'HASH';
+	for my $f ( keys %$topic ) {
+		my $form = $topic->{$f};
+		my $fourtmpl = Text::Template->new( type => 'file',
+			source =>  'oneA4twogroups.tmpl' ,
+			delimiters => [ '<TMPL>', '</TMPL>' ]);
+		my $quiztmpl = Text::Template->new( type => 'file',
+			source =>  'questionsB5.tmpl' ,
+			delimiters => [ '<TMPL>', '</TMPL>' ]);
+		my $fio = io "$ARGV[0]/four$t$f.tex";
+		my $qio = io "$ARGV[0]/quiz$t$f.tex";
+		my $hio = io "$ARGV[0]/quiz$t$f.html";
+		my $n = 1;
+		my $questions = $form->{quiz};
+		for my $qa ( @$questions ) {
+			$form->{ "q$n" } = $qa->{question};
+			$n++;
+		}
+		$fio->print( $fourtmpl->fill_in( hash=> $form ) );
+		$qio->print( $quiztmpl->fill_in( hash=> $form ) );
+		my @htmlq = map { $form->{"q$_"} } 1 .. $n-1;
+		$,="\n<li>";
+		$hio->print("<h2>$cards->{identifier}</h2><ol>", @htmlq);
+	}
+}
