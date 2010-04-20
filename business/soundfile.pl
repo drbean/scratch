029f280f0eb8e2c5a95f305b17c8e262de529ac0
drbean <drbean@users.noreply.github.com>
2010-04-20 00:38:34 +0000

    Net::FTP to ftp to web.nuu. Last 'comp' dictation. perl doesn't understand ~. Replace business with genre var.

--- business/soundfile.pl
+++ business/soundfile.pl
@@ -3,29 +3,39 @@
 use strict;
 use warnings;
 
+my $genre = 'business';
+
+use Net::FTP;
+my $ftp = Net::FTP->new('web.nuu.edu.tw') or
+			die "web.nuu.edu.tw? $@";
+$ftp->login('greg', '1514') or die "web.nuu.edu.tw login? $@";
+$ftp->cwd("public_html/$genre") or die
+	"web.nuu.edu.tw/~greg/public_html? $@";
+$ftp->binary;
+
 use YAML qw/LoadFile DumpFile/;
 my ($texts, $questions) = LoadFile 'dic.yaml';
-my @soundfiles = ( [ 211,
-				"voice_nitech_us_rms_arctic_hts",
-	"compA" ], [ 212,
-				"voice_nitech_us_awb_arctic_hts",
-	"compB" ], [ 213,
-				"voice_nitech_us_clb_arctic_hts",
-	"compC" ], [ 214,
-				"voice_nitech_us_slt_arctic_hts",
+my @soundfiles = ( [ 90,
+						"voice_nitech_us_rms_arctic_hts",
+	"compA" ], [ 91,
+						"voice_nitech_us_awb_arctic_hts",
+	"compB" ], [ 92,
+						"voice_nitech_us_clb_arctic_hts",
+	"compC" ], [ 93,
+						"voice_nitech_us_slt_arctic_hts",
 	"compD" ],
 	);
 
 for ( @soundfiles ) {
 	# my ( $text, $voice, $local ) = @$_;
-	
-	# my $text = io('transcript.txt')->print( "$texts->[$_->[0]]->[4]" );
-	my $text = $questions->[$_->[0]]->[4];
+	my $text = $texts->[$_->[0]]->[4];
 	my $voice = $_->[1];
-	my $local = "~/soundfiles/business/$_->[2].mp3";
+	my $local = "/home/drbean/soundfiles/$genre/$_->[2].mp3";
 	system( "echo \"$text\" |
-		text2wave -eval \"($voice)\" -otype wav -o /tmp/business.wav"
+		text2wave -eval \"($voice)\" -otype wav -o /tmp/$genre.wav"
 		) == 0 or die "text2wave? $@,";
-	system(	"lame -h -V 0 /tmp/business.wav $local" ) == 0 or
+	system(	"lame -h -V 0 /tmp/$genre.wav $local" ) == 0 or
 		die "lame? $@,";
+	$ftp->put($local) or die "put $_->[2].mp3 on web.nuu.edu.tw? $@";
 }
+
