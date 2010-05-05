182387c87e7e7d565239557f5120db73eb6e80b6
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2010-05-05 12:52:41 +0000

    10 compcomp questions for a number of different compcomps with topics, forms. Update to an earlier version than this to more easily generate 4 listening files from dic.yaml.

--- business/soundfile.pl
+++ business/soundfile.pl
@@ -14,28 +14,40 @@ $ftp->cwd("public_html/$genre") or die
 $ftp->binary;
 
 use YAML qw/LoadFile DumpFile/;
-my ($texts, $questions) = LoadFile 'dic.yaml';
-my @soundfiles = ( [ 90,
-						"voice_nitech_us_rms_arctic_hts",
-	"compA" ], [ 91,
-						"voice_nitech_us_awb_arctic_hts",
-	"compB" ], [ 92,
-						"voice_nitech_us_clb_arctic_hts",
-	"compC" ], [ 93,
-						"voice_nitech_us_slt_arctic_hts",
-	"compD" ],
+my ($texts) = LoadFile 'PlanningManaging/cards.yaml';
+my @soundfiles;
+push @soundfiles, (
+			 [ 0, "voice_nitech_us_rms_arctic_hts", "1" ],
+			 [ 1, "voice_nitech_us_awb_arctic_hts", "2" ],
+			 [ 2, "voice_nitech_us_clb_arctic_hts", "3" ],
+			 [ 3, "voice_nitech_us_slt_arctic_hts", "4" ],
+			 [ 4, "voice_nitech_us_rms_arctic_hts", "5" ],
+			 [ 5, "voice_nitech_us_awb_arctic_hts", "6" ],
+			 [ 6, "voice_nitech_us_clb_arctic_hts", "7" ],
+			 [ 7, "voice_nitech_us_slt_arctic_hts", "8" ],
+			 [ 8, "voice_nitech_us_rms_arctic_hts", "9" ],
+			 [ 9, "voice_nitech_us_awb_arctic_hts", "10" ],
 	);
 
-for ( @soundfiles ) {
-	# my ( $text, $voice, $local ) = @$_;
-	my $text = $texts->[$_->[0]]->[4];
-	my $voice = $_->[1];
-	my $local = "/home/drbean/soundfiles/$genre/$_->[2].mp3";
-	system( "echo \"$text\" |
-		text2wave -eval \"($voice)\" -otype wav -o /tmp/$genre.wav"
-		) == 0 or die "text2wave? $@,";
-	system(	"lame -h -V 0 /tmp/$genre.wav $local" ) == 0 or
-		die "lame? $@,";
-	$ftp->put($local) or die "put $_->[2].mp3 on web.nuu.edu.tw? $@";
+for my $topicform (
+	[ qw/kendall influences/ ],
+	[ qw/kendall coventgreen/ ],
+	[ qw/nicholson stealing/ ],
+) {
+
+	my $topic = $topicform->[0];
+	my $form = $topicform->[1];
+	for ( @soundfiles ) {
+		# my ( $text, $voice, $local ) = @$_;
+		my $text = $texts->{$topic}->{compcomp}->{$form}->{quiz}->[$_->[0]]->{question};
+		my $voice = $_->[1];
+		my $local = "/home/drbean/soundfiles/$genre/${topic}_${form}_$_->[2].mp3";
+		system( "echo \"$text\" |
+			text2wave -eval \"($voice)\" -otype wav -o /tmp/$genre.wav"
+			) == 0 or die "text2wave? $@,";
+		system(	"lame -h -V 0 /tmp/$genre.wav $local" ) == 0 or
+			die "lame? $@,";
+		$ftp->put($local) or die "put $_->[2].mp3 on web.nuu.edu.tw? $@";
+	}
 }
 
