b8e74584ceabf883766c9e76afdd912af728176e
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-09-17 11:11:28 +0000

    Mixed model-specific code and non-specific parsing, logical form, evaluating code.

---
+++ conv/marriage/allen/Words.hs
@@ -0,0 +1,17 @@
+import Parsing
+import Data.Char
+import Data.List
+
+characters = sort $ map (\(x:xs) -> (toUpper x) : xs) people_names
+
+otherwords = pronouns ++ interrogatives ++ object_names ++ intransitive_names ++transitives ++ determiners ++ class_names ++ aux ++ ditransitives ++ preps
+
+sortedwords = unlines $ map (
+	\i -> unwords $ [(toUpper i) : ":"] ++
+	[ (l:ls) | (l:ls) <- otherwords, i==l ]
+	) ['a'..'z']
+
+
+main = do
+	putStrLn $ unwords characters
+	putStr sortedwords
