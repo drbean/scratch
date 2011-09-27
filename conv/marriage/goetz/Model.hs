2669fb14c515538d53890c1654d39d7a222c4f26
drbean <drbean@users.noreply.github.com>
2011-09-27 13:54:13 +0000

    interpretation function, int moved closer to where relations are defined in Model. goetz.

--- conv/marriage/goetz/Model.hs
+++ conv/marriage/goetz/Model.hs
@@ -20,14 +20,17 @@ entities	=  [minBound..maxBound]
 characters :: [ (String, Entity) ]
 
 characters = [
-	( "alice",	A ),
-	( "kelley",	K ),
-	( "rex",	X ),
-	( "judy",	J ),
-	( "rock",	R ),
-	( "car_window",	W ),
-	( "story",	S ),
-	( "box",	B )
+	( "pj",	P ),
+	( "sam",	S ),
+	( "dan",	D ),
+	( "dick",	K ),
+	( "bar",	B ),
+	( "fake_id",	I ),
+	( "phone_number",	N ),
+	( "story",	Y ),
+	( "teacher",	T ),
+	( "ann",	A )
+
 
 	]
 
@@ -37,8 +40,8 @@ names = map swap characters
 
 male, female :: OnePlacePred
 
-male	= pred1 [X]
-female	= pred1 [A,K,J]
+male	= pred1 [S,D,K]
+female	= pred1 [P,A]
 
 type OnePlacePred	= Entity -> Bool
 type TwoPlacePred	= Entity -> Entity -> Bool
@@ -55,31 +58,31 @@ people, things :: OnePlacePred
 people	= \ x -> (male x || female x )
 things	= \ x -> not (people x || x == Unspec)
 
-rock	= pred1 [R]
-car_window	= pred1 [W]
-box	= pred1 [B]
-story	= pred1 [S]
-worker	= pred1 [X,J,A]
+bar	= pred1 [B]
+fake_id	= pred1 [I]
+teacher	= pred1 [T]
+child	= pred1 [S]
+phone_number = pred1 [N]
+story	= pred1 [Y]
 
 pred2 xs	= curry ( `elem` xs )
 pred3 xs	= curry3 ( `elem` xs )
 
 --(parent,child)
-parenting	= [ (J,K) ]
+parenting	= [ (D,S),(P,S),(A,P) ]
 --(husband,wife)
-marriages	= [ (X,J) ]
+marriages	= [ (D,P) ]
 --(initiator,wrongdoer?)
-divorces	= [ (J,X) ]
+separations	= [ (D,P) ]
+divorces	= []
 --(boyfriend,girlfriend)
-unmarried_couples	= [ (X,A) ]
+unmarried_couples	= []
 --(contacter,contactee)
-contacts	= [ (X,J) ]
-coworkers	= [ (X,A),(J,Unspec) ]
-putting	= [ (X,R,B) ]
-throwing	= [ (J,R,W) ]
-giving	= [ (X,B,J) ]
-telling	= [ (J,S,K) ]
-boxing	= [ (X,R,B) ]
+possessions	= [ (P,I) ]
+yelling		= [ (P,D) ]
+friendships	= [ (D,K) ]
+--(worker,job)
+work	= [ (P,T),(D,Unspec),(K,Unspec) ]
 			
 parented, married ::  TwoPlacePred
 
@@ -87,33 +90,40 @@ parented	= pred2 parenting
 raised_by	= pred2 $ map swap parenting
 married		= pred2 $ marriages ++ map swap marriages
 divorced	= pred2 divorces
-have	= pred2 $ parenting ++ marriages ++ unmarried_couples
-			++ ( map swap $ parenting ++ marriages ++ unmarried_couples )
-contacted	= pred2 contacts
-worked_with	= pred2 $ coworkers ++ map swap coworkers
+separated	= pred2 separations
+have	= pred2 $ possessions ++ parenting ++ marriages ++ unmarried_couples
+		++ ( map swap $ parenting ++ marriages ++ unmarried_couples )
+knew	= \x y -> ( parented x y || raised_by x y || married x y || divorced x y
+		)
+yelled_at	= pred2 $ yelling ++ map swap yelling
+worked_as	= pred2 work
 
-man	= male
-woman	= female
+boy	= \x -> male x && child x
+man	= \x -> ( not $ boy x ) && male x
+girl	= \x -> ( female x && child x )
+woman	= \x -> ( not $ girl x ) && female x
 parent	= pred1 $ map fst parenting
-child	= pred1 $ map snd parenting
+offspring	= pred1 $ map snd parenting
 mother	= \x -> ( female x && parent x )
 father	= \x -> ( male x && parent x )
-daughter	= \x -> ( female x && child x )
-son	= \x -> ( male x && child x )
+daughter	= \x -> ( female x && offspring x )
+son	= \x -> ( male x && offspring x )
 boyfriend	= pred1 $ map fst unmarried_couples
 girlfriend	= pred1 $ map snd unmarried_couples
+worker	= pred1 $ map fst work
 
 curry3 :: ((a,b,c) -> d) -> a -> b -> c -> d
 curry3 f x y z	= f (x,y,z)
 
-throw, put :: ThreePlacePred
+met :: ThreePlacePred
 
-throw	= pred3 throwing
-put	= pred3 putting
-give	= pred3 giving
-hand	= give
-tell	= pred3 telling
-boxed	= pred3 boxing
+meetings	= [ (D,P,B) ]
+telling	= [ (P,S,Y) ]
+giving	= [ (P,N,D) ]
+
+met	= pred3 meetings
+gave	= pred3 giving
+told	= pred3 telling
 
 passivize :: TwoPlacePred -> OnePlacePred
 passivize r	= \ x -> or ( map ( flip  r x ) entities )
@@ -123,3 +133,46 @@ passivize r	= \ x -> or ( map ( flip  r x ) entities )
 
 self ::  (a -> a -> b) -> a -> b
 self p	= \ x -> p x x 
+
+--evaluation in model
+
+type Interp a	= String -> [a] -> Bool
+
+int :: Interp Entity
+int "man"	= \ [x] -> man x
+int "men"	= \ [x] -> man x
+int "woman"	= \ [x] -> woman x
+int "women"	= \ [x] -> woman x
+
+int "person"	= \ [x] -> people x
+int "thing"	= \ [x]	-> things x
+
+int "parent" = \ [x] -> parent x
+int "parents" = \ [x] -> parent x
+int "mother" = \ [x] -> mother x
+int "father" = \ [x] -> father x
+int "daughter" = \ [x] -> daughter x
+int "boyfriend" = \[x] -> boyfriend x
+int "girlfriend" = \[x] -> girlfriend x
+
+int "phone_number" = \ [x] -> phone_number x
+int "teacher" = \ [x] -> teacher x
+int "fake_id" = \ [x] -> fake_id x
+int "story" = \ [x] -> story x
+int "bar" = \ [x] -> bar x
+
+int "worked" = \ args -> case args of [x] -> worker x; [x,y] -> worked_as y x
+int "work" = int "worked"
+
+int "parented"	= \ [x,y] -> parented y x
+int "married"	= \ [x,y] -> married y x;	int "marry"	= int "married"
+int "divorced"	= \ [x,y] -> divorced y x;	int "divorce"	= int "divorced"
+int "separated"	= \ [x,y] -> separated y x;	int "separate" = int "separated"
+int "had"	= \ [x,y] -> have y x
+int "have"	= \ [x,y] -> have y x
+
+int "met"	= \ [x,y,z] ->	met z y x;	int "meet"	= int "met"
+int "gave"	= \ [x,y,z] ->	gave z y x;	int "give"	= int "gave"
+int "told"	= \ [x,y,z] ->	told z y x;	int "tell"	= int "told"
+
+
