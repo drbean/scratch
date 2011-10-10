80e864e05315ed556430c2e16e3bb36fdce68e94
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-10-10 09:34:22 +0000

    have work, work_as, work_where. taught

--- conv/poverty/rueda/Model.hs
+++ conv/poverty/rueda/Model.hs
@@ -29,7 +29,9 @@ characters = [
 	( "drugs",	H ),
 	( "story",	Y ),
 	( "teacher",	T ),
-	( "school",	L ),
+	( "work",	W ),
+	( "economics",	E ),
+	( "high_school",	L ),
 	( "construction_site",	C )
 
 
@@ -65,10 +67,11 @@ drugs	= pred1 [H]
 stand	= pred1 [S]
 story	= pred1 [Y]
 construction_site	= pred1 [C]
-school	= pred1 [L]
+economics	= pred1 [E]
+high_school	= pred1 [L]
 
 child	= pred1 [N]
-teacher	= pred1 [A]
+teacher	= pred1 [A,T]
 
 cried	= pred1 [M]
 
@@ -85,15 +88,12 @@ parenting	= [ (M,N) ]
 --(boyfriend,girlfriend)
 -- unmarried_couples	= []
 --(contacter,contactee)
-possessions	= [ (N,M),(M,N),(N,D),(N,S),(A,D) ]
---(worker,site)
-work	= [ (N,S),(N,C),(M,Unspec),(A,L) ]
-teaching	= [ (A,N) ]
+possessions	= [ (N,M),(M,N),(N,D),(N,S),(N,O),(A,D) ]
 			
 raised_by	= pred2 $ map swap parenting
-have	= pred2 $ possessions ++ parenting
+have	= pred2 $ possessions ++ parenting 
 		++ ( map swap $ parenting )
-worked_at	= pred2 work
+		++ ( map (\x->(agent x,W) ) working )
 
 boy	= \x -> male x && child x
 man	= \x -> ( not $ boy x ) && male x
@@ -105,7 +105,7 @@ mother	= \x -> ( female x && parent x )
 father	= \x -> ( male x && parent x )
 daughter	= \x -> ( female x && offspring x )
 son	= \x -> ( male x && offspring x )
-worker	= pred1 $ map fst work
+worker	= pred1 $ map agent working
 
 curry3 :: ((a,b,c) -> d) -> a -> b -> c -> d
 curry3 f x y z	= f (x,y,z)
@@ -115,10 +115,14 @@ met :: ThreePlacePred
 meetings	= [ (N,A,L) ]
 telling	= [ (N,S,A) ]
 giving	= [ (N,D,M) ]
+teaching = [ (A,E,N) ]
+--(worker,job,site)
+working	= [ (N,Unspec,S),(N,Unspec,C),(M,Unspec,Unspec),(A,T,L) ]
 
 met	= pred3 meetings
 gave	= pred3 giving
 told	= pred3 telling
+taught	= pred3 teaching
 
 agent, theme, recipient, location :: (Entity,Entity,Entity) -> Entity
 agent (a,_,_) = a
@@ -127,6 +131,10 @@ recipient (_,_,r) = r
 location = recipient
 
 recite = pred2 $ map ( \x -> (agent x, theme x) ) telling
+taught_who = pred2 $ map (\x -> (agent x, recipient x) ) teaching
+taught_what = pred2 $ map (\x -> (agent x, theme x) ) teaching
+work_where	= pred2 $ map (\x -> (agent x, location x) ) working
+work_as = pred2 $ map (\x -> (agent x, theme x) ) working
 
 passivize :: TwoPlacePred -> OnePlacePred
 passivize r	= \ x -> or ( map ( flip  r x ) entities )
@@ -155,6 +163,7 @@ int "parents" = \ [x] -> parent x
 int "mother" = \ [x] -> mother x
 int "father" = \ [x] -> father x
 int "daughter" = \ [x] -> daughter x
+int "son" = \ [x] -> son x
 
 int "money" = \ [x] -> money x
 int "stand" = \ [x] -> stand x
@@ -163,14 +172,20 @@ int "shoes" = \ [x] -> shoes x
 int "drugs" = \ [x] -> drugs x
 int "story" = \ [x] -> story x
 int "construction_site" = \ [x] -> construction_site x
-int "school" = \ [x] -> school x
+int "high_school" = \ [x] -> high_school x
 
-int "worked" = \ args -> case args of [x] -> worker x; [x,y] -> worked_at y x
-int "work" = int "worked"
+int "work" = \args -> case args of
+	[x] -> worker x
+	[x,y] -> work_where y x || work_as y x
+int "worked" = int "work"
+int "had" = \[x,y] -> have y x;	int "have" = int "had"
 
 int "met"	= \ [x,y,z] ->	met z y x;	int "meet"	= int "met"
 int "gave"	= \ [x,y,z] ->	gave z y x;	int "give"	= int "gave"
 int "told" = \ args -> case args of [x,y] -> recite y x; [x,y,z] -> told z y x
 int "tell" = int "told"
-
+int "taught" = \args -> case args of
+	[x,y] -> (taught_who y x || taught_what y x)
+	[x,y,z] -> taught z y x
+int "teach" = int "taught"
 
