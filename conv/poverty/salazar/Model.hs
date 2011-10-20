813df2aeccd6f72e62c704a01d87700a4ab75eaa
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-10-20 01:48:53 +0000

    schooling,4-tuple

--- conv/poverty/salazar/Model.hs
+++ conv/poverty/salazar/Model.hs
@@ -20,20 +20,22 @@ entities	=  [minBound..maxBound]
 characters :: [ (String, Entity) ]
 
 characters = [
-	( "noe",	N ),
-	( "alex",	A ),
-	( "maria",	M ),
-	( "steve",	V ),
+	( "ken",	K ),
+	( "john",	J ),
+	( "leroy",	L ),
+	( "henry",	H ),
+	( "emma",	E ),
 	( "money",	D ),
-	( "stand",	S ),
-	( "shoes",	O ),
-	( "drugs",	H ),
+	( "farm",	F ),
+	( "shoe",	O ),
+	( "toy",	T ),
+	( "bike",	B ),
 	( "story",	Y ),
-	( "teacher",	T ),
 	( "work",	W ),
-	( "economics",	E ),
-	( "high_school",	L ),
-	( "construction_site",	C )
+	( "law",	A ),
+	( "school",	S ),
+	( "colorado_college",	C ),
+	( "michigan_law",	M )
 
 
 	]
@@ -44,8 +46,8 @@ names = map swap characters
 
 male, female :: OnePlacePred
 
-male	= pred1 [N,A,T]
-female	= pred1 [M]
+male	= pred1 [K,J,L,H]
+female	= pred1 [E]
 
 type OnePlacePred	= Entity -> Bool
 type TwoPlacePred	= Entity -> Entity -> Bool
@@ -63,37 +65,38 @@ people	= \ x -> (male x || female x )
 things	= \ x -> not (people x || x == Unspec)
 
 money	= pred1 [D]
-shoes	= pred1 [O]
-drugs	= pred1 [H]
-stand	= pred1 [S]
+shoe	= pred1 [O]
+toy	= pred1 [T]
+bike	= pred1 [B]
+farm	= pred1 [F]
 story	= pred1 [Y]
-construction_site	= pred1 [C]
-economics	= pred1 [E]
-high_school	= pred1 [L]
+law	= pred1 [A]
+school	= pred1 [C,M,S]
+college	= pred1 [C]
+law_school = pred1 [M]
 
-child	= pred1 [N]
-teacher	= pred1 [A,T]
+child	= pred1 [K,J,L]
 
-cried	= pred1 [M]
 
 pred2 xs	= curry ( `elem` xs )
 pred3 xs	= curry3 ( `elem` xs )
+pred4 xs	= curry4 ( `elem` xs )
 
 --(parent,child)
-parenting	= [ (M,N) ]
+parenting	= [  (H,L),(H,J),(H,K),(E,L),(E,J),(E,K) ]
 --(husband,wife)
--- marriages	= [ (D,P) ]
---(initiator,wrongdoer?)
+marriages	= [ (H,E) ]
+--(divorcer,divorced)
 -- separations	= [ (D,P) ]
 -- divorces	= []
 --(boyfriend,girlfriend)
 -- unmarried_couples	= []
 --(contacter,contactee)
-possessions	= [ (N,M),(M,N),(N,D),(N,S),(N,O),(A,D) ]
-			
+possessions	= [ (H,F),(L,O),(J,O) ]
+
 raised_by	= pred2 $ map swap parenting
-have	= pred2 $ possessions ++ parenting 
-		++ ( map swap $ parenting )
+have	= pred2 $ possessions ++ marriages ++ parenting 
+		++ ( map swap $ marriages ++ parenting )
 		++ ( map (\x->(agent x,W) ) working )
 
 boy	= \x -> male x && child x
@@ -110,21 +113,19 @@ worker	= pred1 $ map agent working
 
 curry3 :: ((a,b,c) -> d) -> a -> b -> c -> d
 curry3 f x y z	= f (x,y,z)
+curry4 f x y z w	= f (x,y,z,w)
 
 met :: ThreePlacePred
 
-meetings	= [ (N,A,L) ]
-telling	= [ (N,Y,A) ]
-giving	= [ (N,D,M),(V,D,N) ]
---(teacher,subject,student) or (teacher,school,student)
-teaching = [ (A,E,N),(A,L,N) ]
+meetings	= []
+telling	= [ (K,Y,J),(J,Y,K) ]
+giving	= [ (L,O,J) ]
 --(worker,job,site)
-working	= [ (N,Unspec,S),(N,Unspec,C),(M,Unspec,Unspec),(A,T,L) ]
+working	= [ (H,Unspec,F) ]
 
 met	= pred3 meetings
 gave	= pred3 giving
 told	= pred3 telling
-taught	= pred3 teaching
 
 agent, theme, recipient, location :: (Entity,Entity,Entity) -> Entity
 agent (a,_,_) = a
@@ -133,16 +134,31 @@ recipient (_,_,r) = r
 location = recipient
 
 recite = pred2 $ map ( \x -> (agent x, theme x) ) telling
-taught_who = pred2 $ map (\x -> (agent x, recipient x) ) teaching
-taught_what = pred2 $ map (\x -> (agent x, theme x) ) teaching
 work_where	= pred2 $ map (\x -> (agent x, location x) ) working
 work_as = pred2 $ map (\x -> (agent x, theme x) ) working
+
+--(teacher,school,subject,student)
+schooling = [ (Unspec,C,Unspec,K),(Unspec,M,A,K), (Unspec,Unspec,Unspec,L),
+			(Unspec,Unspec,Unspec,J) ]
+
+agent4, theme4, recipient4, location4 :: (Entity,Entity,Entity,Entity) -> Entity
+agent4 (a,_,_,_) = a
+location4 (_,l,_,_) = l
+theme4 (_,_,t,_) = t
+recipient4 (_,_,_,r) = r
+
+studied = pred3 $ map ( \x -> (location4 x, theme4 x, recipient4 x) )
+				schooling
+studied_what = pred2 $ map (\x -> (recipient4 x, theme4 x) ) schooling
+studied_where = pred2 $ map (\x -> (recipient4 x, location4 x) ) schooling
 
 passivize :: TwoPlacePred -> OnePlacePred
 passivize r	= \ x -> or ( map ( flip  r x ) entities )
 
--- passivize3 :: ThreePlacePred -> OnePlacePred
--- passivize3 r	= \ x -> or ( map ( flip  r x ) entities )
+passivize3 :: ThreePlacePred -> TwoPlacePred
+passivize3 r	= \x y -> or ( map ( \u -> r u x y ) entities )
+
+passivize4 r = \x y z -> or ( map (\u -> r u x y z ) entities )
 
 self ::  (a -> a -> b) -> a -> b
 self p	= \ x -> p x x 
@@ -170,13 +186,10 @@ int "daughter" = \ [x] -> daughter x
 int "son" = \ [x] -> son x
 
 int "money" = \ [x] -> money x
-int "stand" = \ [x] -> stand x
-int "teacher" = \ [x] -> teacher x
-int "shoes" = \ [x] -> shoes x
-int "drugs" = \ [x] -> drugs x
+int "toys" = \ [x] -> toy x; int "toy" = int "toys"
+int "shoes" = \ [x] -> shoe x; int "shoe" = int "shoes"
+int "bikes" = \ [x] -> bike x; int "bike" = int "bikes"
 int "story" = \ [x] -> story x
-int "construction_site" = \ [x] -> construction_site x
-int "high_school" = \ [x] -> high_school x
 
 int "work" = \args -> case args of
 	[x] -> worker x
@@ -188,8 +201,10 @@ int "met"	= \ [x,y,z] ->	met z y x;	int "meet"	= int "met"
 int "gave"	= \ [x,y,z] ->	gave z y x;	int "give"	= int "gave"
 int "told" = \ args -> case args of [x,y] -> recite y x; [x,y,z] -> told z y x
 int "tell" = int "told"
-int "taught" = \args -> case args of
-	[x,y] -> (taught_who y x || taught_what y x)
-	[x,y,z] -> taught z y x
-int "teach" = int "taught"
+int "studied" = \args -> case args of
+	[x,y] -> (studied_where y x || studied_what y x)
+	[x,y,z] -> studied z y x
+int "study" = int "studied"
+int "went" = int "studied"
+int "go" = int "went"
 
