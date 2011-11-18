03ddc88ba1b67ebf88c44dd4d9dd150dadc4475d
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-11-18 01:18:34 +0000

    intercultural-general

---
+++ conv/intercultural/Questioner.hs
@@ -0,0 +1,32 @@
+import Data.Maybe
+import GHC.IO.Handle
+import System.IO
+
+import Parsing
+import LogicalForm
+
+firstparse :: [ParseTree Cat Cat] -> ParseTree Cat Cat
+firstparse [] = Ep
+firstparse (a:as) = a
+
+yesorno :: Answer -> String
+yesorno (Boolean False) = "No"
+yesorno (Boolean True)  = "Yes"
+
+main = do
+	sentence <- getLine
+	let lexed = lexer sentence
+	putStrLn $ unwords lexed
+	let parse = firstparse. parses $ sentence
+	hClose stderr
+	hDuplicateTo stdout stderr
+	let label = case (parse) of
+		Ep -> "Unparseable"
+		_  -> catLabel $ t2c parse
+	putStrLn $ label
+	let answer = transTXT $ parse
+	let response = case label of
+		"YN" -> yesorno $ eval answer
+		_ -> show $ eval answer
+	putStrLn response
+	hClose stdout
