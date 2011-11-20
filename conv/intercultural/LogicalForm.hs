c2243c2f5c64240b61025cb5cd7d3db844c6d09a
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-11-20 04:20:09 +0000

    Single (Term -> LF) used for 'the' transDET. tests out to Tests.hs. falseOut, mapMaybe for testents scope. evals handler.

--- conv/intercultural/LogicalForm.hs
+++ conv/intercultural/LogicalForm.hs
@@ -20,6 +20,7 @@ data LF = NonProposition
         | Disj [LF] 
         | Forall (Term -> LF)
         | Exists (Term -> LF)
+        | Single (Term -> LF)
         | Several (Term -> LF)
         | Many (Term -> LF)
         | Most (Term -> LF)
@@ -49,6 +50,8 @@ ishow (Disj lfs) i    = "disj" ++ "[" ++ ishowForms lfs i ++ "]"
 ishow (Forall scope) i = "Forall x" ++ show i ++ (' ' :
 			(ishow (scope (Var i)) (i+1)))
 ishow (Exists scope) i = "Exists x" ++ show i ++ (' ' :
+			(ishow (scope (Var i)) (i+1)))
+ishow (Single scope) i = "Single x" ++ show i ++ (' ' :
 			(ishow (scope (Var i)) (i+1)))
 ishow (Several scope) i = "Several x" ++ show i ++ (' ' :
 			(ishow (scope (Var i)) (i+1)))
@@ -105,14 +108,16 @@ transDET (Leaf (Cat "a" "DET" _ _)) =
   \ p q -> Exists (\v -> Conj [p v, q v] )
 transDET (Leaf (Cat "zero" "DET" _ _)) = 
   \ p q -> Exists (\v -> Conj [p v, q v] )
+transDET (Leaf (Cat "the" "DET" _ _)) = 
+  \ p q -> Single (\v -> Impl (p v) (q v) )
 transDET (Leaf (Cat "several" "DET" _ _)) = 
   \ p q -> Several (\v -> Impl (p v) (q v) )
 transDET (Leaf (Cat "no" "DET" _ _)) = 
   \ p q -> Neg (Exists (\v -> Conj [p v, q v]))
-transDET (Leaf (Cat "the" "DET" _ _)) = 
-  \ p q -> Exists (\v1 -> Conj
-  		[Forall (\v2 -> Equi (p v2) (Eq v1 v2)),
-		q v1])
+--transDET (Leaf (Cat "the" "DET" _ _)) = 
+--  \ p q -> Exists (\v1 -> Conj
+--  		[Forall (\v2 -> Equi (p v2) (Eq v1 v2)),
+--		q v1])
 transDET (Leaf (Cat "most" "DET" _ _)) = 
   \ p q -> Most (\v -> Impl (p v) (q v) )
 transDET (Leaf (Cat "many" "DET" _ _)) = 
@@ -245,12 +250,14 @@ evl (Conj lfs)	= and ( map ( evl ) lfs )
 evl (Disj lfs)	= or ( map ( evl ) lfs )
 evl (Forall scope)	= and $ testents scope
 evl (Exists scope)	= or $ testents scope
-evl (Several scope)	= length ( filter id $ testents scope ) < 4
-		&& length ( filter id $ testents scope ) > 1
+evl (Single scope)	= singleton ( mapMaybe falseOut $ testents scope )
+evl (Several scope)	= length ( mapMaybe falseOut $ testents scope ) < 4
+		&& length ( mapMaybe falseOut $ testents scope ) > 1
 evl (Many scope)	= length ( filter id $ testents scope ) > 5
 evl (Most scope)	= length ( filter id $ testents scope ) >
 			length ( filter not $ testents scope )
 
+falseOut = \x -> case x of False -> Nothing; True -> Just True 
 testents :: (Term -> LF) -> [Bool]
 testents scope = [ evl (scope (Const e)) | e <- ents ]
 
@@ -269,173 +276,9 @@ singleton :: [a] -> Bool
 singleton [x]	= True
 singleton _	= False
 
-test_text = [
-	"Stephanie spoke Vietnamese.",
-	"Stephanie's brother spoke Vietnamese.",
-	"Stephanie had a brother and a brother spoke Vietnamese.",
-	"Stephanie knew Vietnamese and Theresa spoke English.",
-	"Stephanie spoke Vietnamese but Theresa didn't speak Vietnamese.",
-	"Stephanie talked to Theresa and Theresa talked to Stephanie's father.",
-	"Stephanie talked to Theresa and Theresa talked to Stephanie's father " ++
-		"and Stephanie's father talked to Stephanie.",
-	"Stephanie's brother looked back on Stephanie's upbringing. \
-	\Stephanie talked to Theresa. \
-	\Stephanie asked Theresa about Vietnam."
-	]
-test_possessives = [
-	"Theresa looked back.",
-	"Theresa's daughter looked back.",
-	"Stephanie's father looked back.",
-	"Theresa's daughter looked back on Stephanie's upbringing.",
-	-- "Theresa's daughter looked back on Theresa's daughter's upbringing.",
-	"Stephanie's father looked back on Stephanie's upbringing.",
-	"Did Stephanie's father look back on Stephanie's upbringing?",
-	"Did Stephanie's brother look back on Stephanie's upbringing?",
-	"Did Stephanie's father speak English?",
-	"Did Stephanie's brother speak English?",
-	"Did Stephanie's brother know Vietnamese?",
-	"Did Stephanie's mother speak Vietnamese?",
-	"Did Stephanie's mother speak English?",
-	"Did the sister of Stephanie know Vietnamese?",
-	"Did the father of Stephanie look back on Stephanie's upbringing?",
-	"Did the brother of Stephanie look back on Stephanie's upbringing?",
-	"Did the mother of Stephanie speak English?",
-	"Did the father of Stephanie speak English?",
-	"Did the brother of Stephanie speak English?",
-	"Did the mother of Stephanie speak Vietnamese?",
-	"Did the brother of Stephanie know Vietnamese?",
-	"Did the sister of Stephanie know Vietnamese?"
-	]
-haves = [
-	"Did Ken have Henry?",
-	"Did Ken have John?",
-	"Did Ken have a mother?",
-	"Did Ken have a son?",
-	"Did Ken have a daughter?",
-	"Did Henry have a mother?",
-	"Did Ken have shoes?",
-	"Did Ken have some shoes?",
-	"Did Ken have money?",
-	"Did Henry have money?",
-	"Did John have money?",
-	"Did Ken have a parent?",
-	"Did Ken have some parents?",
-	"Did Ken have parents?",
-	"Did Henry have a parent?",
-	"Did Henry have some parents?",
-	"Did Henry have parents?",
-	"Did Ken have look back?",
-	"Did John have look back?",
-	"Did Henry have look back?"
-	]
-ungrammatical = [
-	"Did John looked back?",
-	"Ken look back?",
-	"Man looked back.",
-	"Some man work.",
-	"No looked back.",
-	"No-one work.",
-	"Did John teach?",
-	"John teach Ken.",
-	"Ken taught."
-	]
-intransitives = [
-	"Did Stephanie look back?",
-	"Did Theresa look back?",
-	"Did Stephanie's father look back?",
-	"A man looked back.",
-	"Some man looked back.",
-	"No one looked back.",
-	"No-one looked back.",
-	"Everybody looked back.",
-	"Everyone looked back.",
-	-- "Many persons looked back.",
-	"No person looked back.",
-	"Did the man look back?",
-	"Did some man look back?",
-	"Did some men look back?",
-	"Did some woman look back?",
-	"Did some women look back?",
-	"Most men looked back.",
-	"Most men didn't look back.",
-	"Several men looked back.",
-	"Several men didn't look back.",
-	"Many men looked back.",
-	"Many men didn't work.",
-	"All men looked back.",
-	"No man looked back.",
-	"Did Henry look back at a farm?",
-	"Henry looked back on a farm?",
-	"Henry looked back in a farm?"
-	]
-transitives = [
-	"Did John study law?",
-	"John studied law.",
-	"Ken studied law.",
-	"Ken studied law at Michigan Law.",
-	"Ken studied law at Colorado College",
-	"Did Ken go to Colorado College.",
-	"Some woman went to Colorado College.",
-	"Some man went to Colorado College.",
-	"Some boy went to Colorado College.",
-	"Some man parented Ken.",
-	"A man parented John",
-	"Some woman told a story."
-	]
-ditransitive_tests = [
-	"Ken told a story.",
-	"Ken told Henry a story.",
-	"Ken told a story to Henry.",
-	"Ken told a story to John",
-	"Ken gave some shoes to John.",
-	"Did Ken give some shoes to John.",
-	"Did Ken give the shoes to John?",
-	"Did Ken give the shoes to someone?",
-	"Ken gave several shoes to John.",
-	"Did someone give something to John?",
-	"A woman gave the shoes to John.",
-	"A woman gave the shoes to someone.",
-	"A woman gave something to someone.",
-	"Someone gave something to someone.",
-	"Ken gave John some shoes.",
-	"Did Ken give John some shoes?",
-	"Did Ken give John the shoes?",
-	"Did Ken give someone the shoes?",
-	"Ken gave John several shoes.",
-	"Did someone give John something?",
-	"A man gave John the shoes.",
-	"A boy gave John the shoes.",
-	"Leroy gave John the shoe.",
-	"A man gave someone the shoes.",
-	"A man gave someone something.",
-	"Someone gave someone something."
-	]
-wh_questions =[
-	"Who looked back?",
-	"Who did John teach?",
-	"Who taught John?",
-	"Who gave the shoes to John?",
-	"Who gave some shoes to John?",
-	"Which person looked back?",
-	"Which person did John teach?",
-	"To whom did Ken give some shoes?",
-	"Who did Ken give some shoes to?"
-	]
-relclauses = [
-	"A woman who taught John looked back.",
-	"The woman who taught John looked back.",
-	"Did the woman who taught John look back?",
-	"Did every woman who taught John look back?",
-	"The woman who gave the shoes to John looked back.",
-	"Ken divorced the man that she gave the shoes to.",
-	"Who killed the man that helped the woman " 
-	 ++ "that had a boyfriend."
-	]
-
-
 handler core tests = putStr $ unlines $ map (\(x,y) -> x++show y) $ zip (map (++"\t") tests ) ( map core tests )
 
--- evals tests = putStr $ unlines $ map (\(x,y)->x++show y) $ zip (map (++"\t") tests ) ( map (eval . head . process) tests )
+evals = handler (eval . transTXT . head . parses)
 
 forms tests = putStr $ unlines $ map (\(x,y)->x++show y) $ zip (map (++"\t") tests ) ( map process tests )
 
