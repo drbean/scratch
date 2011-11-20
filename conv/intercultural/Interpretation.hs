dd8013c4d67fa4096be4d9e23664d3aae1f63ebe
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-11-20 02:58:08 +0000

    work,left,immigrate,bought,went,came,doll,motel,boat,fields,tomato

--- conv/intercultural/Interpretation.hs
+++ conv/intercultural/Interpretation.hs
@@ -33,6 +33,10 @@ int "brother" = \ [x] -> brother x
 
 int "disappointed" = \[x,y] -> disappoint y x
 int "disappoint" = int "disappointed"
+int "work" = \args -> case args of
+	[x] -> worker x
+	[x,y] -> work_where y x || work_as y x
+int "worked" = int "work"
 int "had" = \[x,y] -> have y x;	int "have" = int "had"
 int "knew" = \[x,y] -> know y x; int "know" = int "knew"
 int "look_back" = \args -> case args of
@@ -49,10 +53,21 @@ int "talked" = \args -> case args of
 	[x,y,z] -> talk_about z y x
 int "talk" = int "talked"
 int "said" = \[x,y] -> said y x; int "say" = int "said"
-int "came" = \[x,y] -> come_from y x; int "come" = int "came"
+int "left" = \args -> case args of
+	[x] -> isImmigrant x
+	[x,y] -> come_from y x
+	[x,y,z] -> immigrate z y x
+int "leave" = int "left"
+int "immigrate" = \args -> case args of
+	[x] -> isImmigrant x
+	[x,y] -> go_to y x
+	[x,y,z] -> immigrate z y x
+
 
 int "gave"	= \ [x,y,z] ->	gave z y x;	int "give"	= int "gave"
 int "got" = \[x,y,z] -> gave x y z; int "get" = int "got"
+int "bought" = int "got"; int "buy" = int "get"
+
 int "accepted" = int "got"; int "accept" = int "accepted"
 int "told" = \ args -> case args of [x,y] -> recite y x; [x,y,z] -> told z y x
 int "tell" = int "told"
@@ -60,8 +75,8 @@ int "studied" = \args -> case args of
 	[x,y] -> (studied_where y x || studied_what y x)
 	[x,y,z] -> studied z y x
 int "study" = int "studied"
-int "went" = int "studied"
-int "go" = int "went"
+int "went" = \[x,y] -> go_to y x; int "go" = int "went"
+int "came" = \[x,y] -> come_from y x; int "come" = int "came"
 
 int "culture" = \[x] -> culture x
 int "upbringing"	= \[x] -> upbringing x
@@ -69,3 +84,10 @@ int "necklace"	= \[x] -> necklace x
 int "present"	= \[x] -> present x
 int "sense"	= \[x] -> sense x
 int "story"	= \[x] -> story x
+int "doll" = \ [x] -> doll x;	int "dolls" = int "doll"
+int "motel" = \ [x] -> motel x
+int "boat" = \ [x] -> boat x
+int "fields" = \ [x] -> fields x
+int "tomato" = \ [x] -> tomato x; int "tomatoes" = int "tomato"
+
+
