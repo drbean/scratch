6133cb6157b231ba1bd163527cad4cd69fab524e
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2011-12-03 08:09:05 +0000

    top-level dir

---
+++ conv/Questioner.hs
@@ -0,0 +1,35 @@
+import Data.Maybe
+import GHC.IO.Handle
+import System.IO
+
+import Parsing
+import LogicalForm
+
+firstparse :: [ParseTree Cat Cat] -> ParseTree Cat Cat
+firstparse [] = Ep
+firstparse (a:as) = a
+
+yesorno :: Answer -> String
+yesorno (Boolean False) = "No"
+yesorno (Boolean True)  = "Yes"
+
+main = do
+	sentence <- getLine
+	let lexed = lexer sentence
+	putStrLn $ unwords lexed
+	let parse = firstparse. parses $ sentence
+	hClose stderr
+	hDuplicateTo stdout stderr
+	let label = case (parse) of
+		Ep -> "Unparseable"
+		_  -> catLabel $ t2c parse
+	putStrLn $ label
+	let answer = case label of
+		"WH" -> transWH $ parse
+		_ -> transTXT $ parse
+	let response = case label of
+		"WH" -> show $ map ( toupper . named) $ evalW answer
+		"YN" -> yesorno $ eval answer
+		"S" -> show $ eval answer
+	putStrLn response
+	hClose stdout
