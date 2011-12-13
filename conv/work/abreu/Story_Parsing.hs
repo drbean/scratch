cf29c1218ae00f6a5b454cb4881cebae8f5f4a69
drbean <drbean@users.noreply.github.com>
2011-12-13 13:37:37 +0000

    abreu

--- conv/work/abreu/Story_Parsing.hs
+++ conv/work/abreu/Story_Parsing.hs
@@ -1,57 +1,53 @@
-module Story_Parsing where
+module StoryCats where
 
 import Parsing
-
-collect_lex = [
-	("auxiliary verbs",	aux),
-	("interesting verbs",	story_verbs),
-	("intransitive verbs",	intransitives),
-	("transitive verbs",	transitives),
-	("ditransitive verbs",	ditransitives),
-	("object_names",	object_names),
-	("class_names",	class_names),
-	("prepositions",	preps),
-	("determiners",	determiners),
-	("possessives",	possessives)
-	]
 
 proper_names, object_names :: Lexset
 
 proper_names = [
 	[Cat "english" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "dee"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "alf"	"NP" [Thrd,Masc,Sg] []]
+	[Cat "spanish" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "the_dominican_republic" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "the_united_states" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "boston" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "boston_university" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "joan"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "adela"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "claritza"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "john_doe"	"NP" [Thrd,Masc,Sg] []]
 	]
 
 object_names = [
 
-	[Cat "uncle"	"NP" [Thrd,Masc,Sg] []],
-	[Cat "nephew"	"NP" [Thrd,Masc,Sg] []],
-
-	[Cat "superintendent"	"NP" [Thrd,Masc,Sg] []],
-	[Cat "supervisor"	"NP" [Thrd,Masc,Sg] []],
-	[Cat "apprentice"	"NP" [Thrd,MascOrFem,Sg] []],
-	[Cat "vocational_school"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "construction"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "electrician"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "electricians"	"NP" [Thrd,Neutr,Pl] []],
-	[Cat "interviewer"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "transformer"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "ship"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "ships"	"NP" [Thrd,Neutr,Pl] []],
-	[Cat "shipyard"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "disappointment"	"NP" [Thrd,Neutr,Sg] []],
 	[Cat "money"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "upbringing"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "story"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "job"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "language"	"NP" [Thrd,Neutr,Sg] []]
+	[Cat "information_technology"	"NP" [Thrd,Neutr,Sg] []],
+
+	[Cat "aunt"	"CN" [Thrd,Fem,Sg] []],
+	[Cat "niece"	"CN" [Thrd,Fem,Sg] []],
+	[Cat "uncle"	"CN" [Thrd,Masc,Sg] []],
+	[Cat "nephew"	"CN" [Thrd,Masc,Sg] []],
+
+	[Cat "boss"	"CN" [Thrd,Fem,Sg] []],
+	[Cat "supervisor"	"CN" [Thrd,Fem,Sg] []],
+	[Cat "receptionist"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "receptionists"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "customer"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "customers"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "visitor"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "visitors"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "story"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "job"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "upbringing"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "name"	"CN" [Thrd,Neutr,Sg] []]
+	[Cat "names"	"CN" [Thrd,Neutr,PlSg] []]
+	[Cat "language"	"CN" [Thrd,Neutr,Sg] []]
 	]
 
 story_verbs = [
-	[Cat "lift"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "resented"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "resent"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]]
+	[Cat "greeted"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "greet"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "spelled"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "spell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]]
 	]
 
 
