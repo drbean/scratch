a559f742345a1fc3944aa95572bae2d4585c3c17
drbean <drbean@users.noreply.github.com>
2011-12-13 09:34:52 +0000

    story-specific interpretations

---
+++ conv/work/abreu/Story_Interpretation.hs
@@ -0,0 +1,23 @@
+module Story_Interpretation where 
+
+import Model
+
+story_objects = [
+	( "superintendent",	\[x] -> superintendent x	),
+	( "supervisor",	\[x] -> supervisor x	),
+	( "husband",	\[x] -> husband x	),
+	( "vocational_school",	\[x] -> vocational_school x	),
+	( "construction",	\[x] -> construction x	),
+	( "electrician",	\[x] -> electrician x	),
+	-- ( "electricians",	\[x] -> electricians x	),
+	( "interviewer",	\[x] -> interviewer x	),
+	( "transformer",	\[x] -> transformer x	),
+	( "ship",	\[x] -> ship x	),
+	( "shipyard",	\[x] -> shipyard x	),
+	( "disappointment",	\[x] -> disappointment x	),
+	( "money",	\[x] -> money x	),
+	( "upbringing",	\[x] -> upbringing x	),
+	( "story",	\[x] -> story x	),
+	( "job",	\[x] -> job x	)
+	]
+
