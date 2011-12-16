93d9725c7289ce0bf55374da52470f1e3f8d5e1a
drbean <drbean@users.noreply.github.com>
2011-12-16 02:16:23 +0000

    story_inflections,story_relations

--- conv/work/abreu/Story_Interpretation.hs
+++ conv/work/abreu/Story_Interpretation.hs
@@ -3,21 +3,34 @@ module Story_Interpretation where
 import Model
 
 story_objects = [
-	( "superintendent",	\[x] -> superintendent x	),
-	( "supervisor",	\[x] -> supervisor x	),
-	( "husband",	\[x] -> husband x	),
-	( "vocational_school",	\[x] -> vocational_school x	),
-	( "construction",	\[x] -> construction x	),
-	( "electrician",	\[x] -> electrician x	),
-	-- ( "electricians",	\[x] -> electricians x	),
-	( "interviewer",	\[x] -> interviewer x	),
-	( "transformer",	\[x] -> transformer x	),
-	( "ship",	\[x] -> ship x	),
-	( "shipyard",	\[x] -> shipyard x	),
-	( "disappointment",	\[x] -> disappointment x	),
+	( "boss",	\[x] -> boss x	),
+	( "company",	\[x] -> company x	),
+	( "receptionist",	\[x] -> receptionist x	),
+	( "customer",	\[x] -> customer x	),
+	( "hospital",	\[x] -> hospital x	),
+	( "company",	\[x] -> company x	),
+	( "visitor",	\[x] -> visitor x	),
+	( "school",	\[x] -> school x	),
+
+	( "i_t",	\[x] -> i_t x	),
+
 	( "money",	\[x] -> money x	),
-	( "upbringing",	\[x] -> upbringing x	),
 	( "story",	\[x] -> story x	),
+	( "aunt",	\[x] -> aunt x	),
+	( "niece",	\[x] -> niece x	),
 	( "job",	\[x] -> job x	)
+	]
+
+story_inflections = [
+ ( "supervisor", "boss" ),
+ ( "information_technology", "i_t" ),
+ ( "customers", "customer" ),
+ ( "visitor", "customer" ),
+ ( "visitors", "visitor" ),
+ ( "greeted", "greet" )
+ ]
+
+story_relations = [
+	( "greet",	\[x,y] -> greet x y	)
 	]
 
