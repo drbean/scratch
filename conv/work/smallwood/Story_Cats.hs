16c619e7fe384d36f17738cf50cc6bf08fad96f1
drbean <drbean@users.noreply.github.com>
2011-12-18 09:00:02 +0000

    treatment,schooling-focused,red,offensive,interview,visit

--- conv/work/smallwood/Story_Cats.hs
+++ conv/work/smallwood/Story_Cats.hs
@@ -5,36 +5,30 @@ import Parsing
 proper_names, object_names, story_verbs :: Lexset
 
 proper_names = [
-	[Cat "english" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "spanish" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "the_dominican_republic" "NP" [Thrd,Neutr,Sg] []],
 	[Cat "the_united_states" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "boston" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "boston_university" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "joan"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "adela"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "claritza"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "john_doe"	"NP" [Thrd,Masc,Sg] []]
+	[Cat "rutgers_university" "NP" [Thrd,Neutr,Sg] []],
+	[Cat "tia"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "christine"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "steven"	"NP" [Thrd,Masc,Sg] []],
+	[Cat "mr_payne"	"NP" [Thrd,Masc,Sg] []],
+	[Cat "mr_batchelor"	"NP" [Thrd,Masc,Sg] []]
 	]
 
 object_names = [
 
 	[Cat "money"	"NP" [Thrd,Neutr,Sg] [],
 		Cat "money"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "information_technology"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "i_t"	"NP" [Thrd,Neutr,Sg] []],
-
-	[Cat "aunt"	"CN" [Thrd,Fem,Sg] []],
-	[Cat "niece"	"CN" [Thrd,Fem,Sg] []],
-	[Cat "uncle"	"CN" [Thrd,Masc,Sg] []],
-	[Cat "nephew"	"CN" [Thrd,Masc,Sg] []],
+	[Cat "dress"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "dresses"	"CN" [Thrd,Neutr,Pl] []],
 
 	[Cat "boss"	"CN" [Thrd,Fem,Sg] []],
 	[Cat "supervisor"	"CN" [Thrd,Fem,Sg] []],
 	[Cat "receptionist"	"CN" [Thrd,MascOrFem,Sg] []],
 	[Cat "receptionists"	"CN" [Thrd,MascOrFem,Pl] []],
-	[Cat "customer"	"CN" [Thrd,MascOrFem,Sg] []],
-	[Cat "customers"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "interviewer"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "interviewers"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "interviewee"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "interviewees"	"CN" [Thrd,MascOrFem,Pl] []],
 	[Cat "visitor"	"CN" [Thrd,MascOrFem,Sg] []],
 	[Cat "visitors"	"CN" [Thrd,MascOrFem,Pl] []],
 	[Cat "company"	"CN" [Thrd,Neutr,Sg] []],
@@ -42,7 +36,9 @@ object_names = [
 	[Cat "school"	"NP" [Thrd,Neutr,Sg] []],
 	[Cat "story"	"CN" [Thrd,Neutr,Sg] []],
 	[Cat "job"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "upbringing"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "finance"	"NP" [Thrd,Neutr,Sg] []],
+	[Cat "accounting"	"NP" [Thrd,Neutr,Sg] []],
+	[Cat "business_law"	"NP" [Thrd,Neutr,Sg] []],
 	[Cat "name"	"CN" [Thrd,Neutr,Sg] []],
 	[Cat "names"	"CN" [Thrd,Neutr,Pl] []],
 	[Cat "language"	"CN" [Thrd,Neutr,Sg] []]
@@ -53,10 +49,21 @@ story_verbs = [
 		Cat "visited"	"VP" [Tense] []],
 	[Cat "visit"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
 		Cat "visit"	"VP" [Infl] []],
+	[Cat "angered"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "anger"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "offended"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "offend"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "interviewed"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "interview"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "greeted"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "greet"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "spelled"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "spell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]]
+	[Cat "wore"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "wear"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]]
+	]
+
+story_adjs = [
+	[Cat "offensive"	"ADJ" [] []],
+	[Cat "red"	"ADJ" [] []]
 	]
 
 
