28659c8d629b4c6ef40f4cb624f6a4cabadfa327
drbean <drbean@users.noreply.github.com>
2011-12-18 08:59:10 +0000

    treatment,schooling-focused,red,offensive

--- conv/work/smallwood/Story_Interpretation.hs
+++ conv/work/smallwood/Story_Interpretation.hs
@@ -5,38 +5,52 @@ import Model
 story_objects = [
 	( "boss",	\[x] -> boss x	),
 	( "company",	\[x] -> company x	),
-	( "receptionist",	\[x] -> receptionist x	),
-	( "customer",	\[x] -> customer x	),
-	( "hospital",	\[x] -> hospital x	),
-	( "company",	\[x] -> company x	),
+	( "interviewee",	\[x] -> interviewee x	),
 	( "visitor",	\[x] -> visitor x	),
 	( "school",	\[x] -> school x	),
+	( "subject",	\[x] -> subject x	),
+	( "language",	\[x] -> language x	),
+	( "request",	\[x] -> request x	),
+	( "treatment",	\[x] -> treatment x	),
 
-	( "i_t",	\[x] -> i_t x	),
 
-	( "name",	\[x] -> name x	),
 	( "money",	\[x] -> money x	),
 	( "story",	\[x] -> story x	),
-	( "aunt",	\[x] -> aunt x	),
-	( "niece",	\[x] -> niece x	),
+	( "dress",	\[x] -> dress x	),
 	( "job",	\[x] -> job x	)
 	]
 
 story_inflections = [
  ( "supervisor", "boss" ),
  ( "information_technology", "i_t" ),
- ( "customers", "customer" ),
  ( "visitor", "customer" ),
  ( "visitors", "visitor" ),
  ( "names", "name" ),
- ( "spelled", "spell" ),
+ ( "wear", "wore" ),
  ( "visited", "visit" ),
+ ( "interviewed", "interview" ),
+ ( "interviewees", "interviewee" ),
+ ( "taught", "teach" ),
+ ( "subjects", "subject" ),
+ ( "offended", "offend" ),
+ ( "angered", "anger"),
  ( "greeted", "greet" )
  ]
 
 story_relations = [
-	( "spell",	\[x,y] -> spell y x	),
-	( "visit",	\[x,y] -> visit y x	),
-	( "greet",	\[x,y] -> greet y x	)
+	( "red",	\[x]	-> red x	),
+	( "offensive",	\[x]	-> offensive x	),
+	( "mean",	\[x]	-> mean x	),
+	( "unfair",	\[x]	-> unfair x	),
+
+	( "wore",	\[x,y]	-> wore y x	),
+	( "visit",	\[x,y]	-> visit y x	),
+	( "interview",	\[x,y]	-> interview y x	),
+	( "teach",	\args -> case args of
+		[x,y] -> (teach_who y x || teach_what y x)
+		[x,y,z] -> teach z y x ),
+	( "offend",	\[x,y]	-> offend y x	),
+	( "anger",	\[x,y]	-> anger y x	),
+	( "greet",	\[x,y]	-> greet y x	)
 	]
 
