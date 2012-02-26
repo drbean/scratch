fcff0d24722920701cb7496c3785cfeebb170a7d
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-02-26 05:16:38 +0000

    From women at work to disability.

---
+++ conv/disability/connell/Model.hs
@@ -0,0 +1,241 @@
+module Model where 
+
+import Data.Tuple
+import Data.List
+import Data.Maybe
+
+data Entity	= A | B | C | D | E | F | G 
+            | H | I | J | K | L | M | N 
+            | O | P | Q | R | S | T | U 
+            | V | W | X | Y | Z | Someone | Something | Unspec
+     deriving (Eq,Show,Bounded,Enum,Ord)
+
+entities :: [Entity]
+entities	=  [minBound..maxBound] 
+
+
+--alice, rex, kelley, judy, rock
+--                                                :: Entity
+
+characters :: [ (String, Entity) ]
+
+characters = [
+	( "ann",	A ),
+	( "the_state_of_colorado",	C ),
+	( "debra",	D ),
+	( "jennifer",	J ),
+	( "the_gathering_place",	P ),
+	( "queen",	Q ),
+	( "tanya",	T ),
+	( "nobody_or_nothing",	Z )
+
+	]
+
+
+white	= pred1 [A,T,D]
+bill	= pred1 [B]
+home	= pred1 [E]
+smell	= pred1 [F]
+gift	= pred1 [G]
+shelter	= pred1 [H]
+sign	= pred1 [I]
+job	= pred1 [J]
+unemployment	= pred1 [L]
+money	= pred1 [M]
+rent	= pred1 [N]
+man	= pred1 [O]
+black	= pred1 [Q]
+cried = pred1 [Q,A]
+homeless	= pred1 [Q,A]
+officewear	= pred1 [R]
+press	= pred1 [S]
+beautiful	= pred1 [U]
+counseling	= pred1 [V]
+spirits	= pred1 [W]
+card	= pred1 [X]
+story	= pred1 [Y]
+
+older	= homeless
+admin	= job
+donator	= man
+lotion	= gift
+
+namelist = map fst characters
+
+
+male, female :: OnePlacePred
+
+child	= pred1 []
+male	= pred1 [O]
+female	= pred1 [Q,A,D,T,J]
+
+type OnePlacePred	= Entity -> Bool
+type TwoPlacePred	= Entity -> Entity -> Bool
+type ThreePlacePred	= Entity -> Entity -> Entity -> Bool
+
+list2OnePlacePred :: [Entity] -> OnePlacePred
+list2OnePlacePred xs	= \ x -> elem x xs
+
+pred1 :: [Entity] -> OnePlacePred
+pred1	= flip elem
+
+people, things :: OnePlacePred
+
+people	= \ x -> (male x || female x || x == Unspec)
+things	= \ x -> (x == Unspec || not ( people x ) )
+
+boy	= \x -> male x && child x
+isMan	= \x -> ( not $ boy x ) && male x
+isGirl	= \x -> ( female x && child x )
+isWoman	= \x -> ( not $ isGirl x ) && female x
+isParent	= pred1 $ map fst parenting
+isOffspring	= pred1 $ map snd parenting
+isMother	= \x -> ( female x && isParent x )
+father	= \x -> ( male x && isParent x )
+daughter	= \x -> ( female x && isOffspring x )
+son	= \x -> ( male x && isOffspring x )
+interviewee = pred1 $ map patient recruitment
+supervisor = pred1 $ map fst supervision
+visitor = interviewee
+
+pred2 :: [(Entity,Entity)] -> TwoPlacePred
+pred3 :: [(Entity,Entity,Entity)] -> ThreePlacePred
+pred2 xs	= curry ( `elem` xs )
+pred3 xs	= curry3 ( `elem` xs )
+pred4 xs	= curry4 ( `elem` xs )
+
+--(parent,child)
+parenting	= [(Q,J)]
+supervision	= [(T,A),(D,Q),(D,A)]
+marriages	= [(Unspec,Q)]
+--(husband,wife,wedding_location)
+weddings	= []
+--(divorcer,divorced)
+separations	= [(Q,Unspec)]
+divorces	= [(Q,Unspec)]
+--(boyfriend,girlfriend)
+-- unmarried_couples	= []
+--(contacter,contactee)
+possessions	= [(O,M),(J,M),(D,M),(T,M)] ++ clothing
+recruitment	= [(C,Q,V)]
+appreciation	= []
+
+raised_by	= pred2 $ map swap parenting
+parented	= pred2 parenting
+marry_in	= pred3 $ weddings ++ map (\(x,y,z) -> (y,x,z) ) weddings
+married		= forgetful marry_in
+separated	= pred2 separations
+wedded_in	= pred2 $ map (\x -> (agent x, location x) ) weddings ++
+			map (\x -> (patient x, location x) ) weddings
+isMarried	= pred1 $ map fst marriages ++ map snd marriages
+parentMaybe :: Entity -> (Entity,Entity) -> Maybe Entity
+parentMaybe child = \rel -> if child == snd rel then Just (fst rel) else Nothing
+parents		= \child -> mapMaybe (parentMaybe child) parenting
+isSiblings	= \a b -> (any . flip elem) (parents a) (parents b)
+brother	= \x -> any ( \i -> isSiblings x i ) entities
+
+clothing	= [(Q,R)]
+losses	= [(Q,J),(Q,E),(A,J),(A,E)]
+looking	= []
+wore	= pred2 clothing
+have	= pred2 $ possessions ++ marriages ++ parenting ++ supervision
+		++ ( map swap $ marriages ++ parenting ++ supervision )
+		++ ( map (\x->(recipient x, theme x) ) giving )
+		++ ( map (\x->(agent x,J) ) working )
+		++ ( map (\x->(agent x, patient x) ) recruitment )
+		++ ( map (\x->(agent x, location x) ) recruitment )
+knowledge	= []
+acquaintances	= []
+know	= pred2 $ knowledge ++ acquaintances ++ map swap acquaintances
+appreciate	= pred2 appreciation
+-- visit	= pred2 $ map (\x -> (patient x, recipient x) ) recruitment
+interview	= pred2 $ map (\x -> (agent x, patient x) ) recruitment
+-- greet	= interview
+look_at	= pred2 $ looking
+lose	= pred2 $ losses
+help	= pred2 $ supervision
+
+curry3 :: ((a,b,c) -> d) -> a -> b -> c -> d
+curry3 f x y z	= f (x,y,z)
+curry4 f x y z w	= f (x,y,z,w)
+
+agent, theme, recipient, location, instrument ::
+	(Entity,Entity,Entity) -> Entity
+agent (a,_,_) = a
+theme (_,t,_) = t
+recipient (_,_,r) = r
+patient = theme
+location = recipient
+instrument = recipient
+origin	= theme
+destination = recipient
+
+--(worker,job,site)
+working	= [(Q,V,C),(T,S,P),(D,H,P),(O,Unspec,Unspec)]
+volunteering = [(Q,Unspec,P),(A,Unspec,P)]
+comms	= [ (Q,Y,D),(A,Y,T),(D,Q,J) ]
+offenses	= []
+giving	= [ (C,L,Q),(O,B,Q),(Q,X,J),(P,G,Q),(P,E,Q) ]
+acceptances = []
+-- (seller, item, buyer)
+selling	= []
+--(killer,killed,instrument)
+--(putter,theme,location)
+--(agent,theme,location)
+looking_back	= [(Q,Unspec,Unspec)]
+
+worker	= pred1 $ map agent working
+work_where	= pred2 $ map (\x -> (agent x, location x) ) working
+work_as = pred2 $ map (\x -> (agent x, theme x) ) working
+volunteer_at	= pred2 $ map (\x -> (agent x, location x) ) volunteering
+look_back	= pred1 $ map agent looking_back
+look_back_on	= pred2 $ map (\x->(agent x, theme x) ) looking_back
+said	= pred2 $ map (\x->(agent x, theme x) ) comms
+asked	= pred2 $ map (\x->(agent x, recipient x) ) comms
+ask_about = pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
+talked	= pred2 $ map (\x->(agent x, recipient x) ) comms
+              ++  map (\(agent,theme,recipient)->(recipient, agent) ) comms
+talk_about = pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
+offend_with	= pred3 offenses
+offend	= pred2 $ ( map (\x -> (agent x, recipient x) ) offenses ) ++
+		( map (\x -> (theme x, recipient x) ) offenses )
+anger = offend
+
+
+gave	= pred3 giving
+got	= pred3 $ map (\x -> (recipient x, patient x, agent x) ) giving
+sold	= pred2 $ map (\x -> (agent x, theme x) ) selling
+
+told	= pred3 comms
+
+recite = pred2 $ map ( \x -> (agent x, theme x) ) comms
+
+agent4, theme4, recipient4, location4 :: (Entity,Entity,Entity,Entity) -> Entity
+agent4 (a,_,_,_) = a
+location4 (_,l,_,_) = l
+theme4 (_,_,t,_) = t
+recipient4 (_,_,_,r) = r
+
+-- (teacher,school(location),subject,student)
+schooling = [(Unspec,Unspec,V,D)]
+studied = pred3 $ map ( \x -> (recipient4 x, theme4 x, location4 x) )
+				schooling
+studied_what = pred2 $ map (\x -> (recipient4 x, theme4 x) ) schooling
+studied_where = pred2 $ map (\x -> (recipient4 x, location4 x) ) schooling
+teach = pred3 $ map (\x -> (agent4 x, theme4 x, recipient4 x) ) schooling
+teach_what = forgetful teach
+teach_who = pred2 $ map (\x -> (agent4 x, recipient4 x) ) schooling
+student = pred1 $ map recipient4 schooling
+
+forgetful :: ThreePlacePred -> TwoPlacePred
+forgetful r u v = or ( map ( r u v ) entities )
+passivize :: TwoPlacePred -> OnePlacePred
+passivize r	= \ x -> or ( map ( flip  r x ) entities )
+
+passivize3 :: ThreePlacePred -> TwoPlacePred
+passivize3 r	= \x y -> or ( map ( \u -> r u x y ) entities )
+
+passivize4 r = \x y z -> or ( map (\u -> r u x y z ) entities )
+
+self ::  (a -> a -> b) -> a -> b
+self p	= \ x -> p x x 
---
+++ conv/disability/connell/Story_Interpretation.hs
@@ -0,0 +1,78 @@
+module Story_Interpretation where 
+
+import Model
+
+story_objects = [
+	( "rent",	\[x]	-> rent x	),
+	( "home",	\[x]	-> home x	),
+	( "unemployment",	\[x]	-> unemployment x	),
+	( "administrative_assistant",	\[x]	-> admin x	),
+	( "officewear",	\[x]	-> officewear x	),
+	( "donator",	\[x]	-> donator x	),
+	( "ten_dollar_bill",	\[x]	-> bill x	),
+	( "black",	\[x]	-> black x	),
+	( "white",	\[x]	-> white x	),
+	( "birthday_card",	\[x]	-> card x	),
+	( "sign",	\[x]	-> sign x	),
+	( "shelter",	\[x]	-> shelter x	),
+	( "gift",	\[x]	-> gift x	),
+	( "smell",	\[x]	-> smell x	),
+
+
+
+
+
+
+	( "supervisor",	\[x]	-> supervisor x	),
+	( "interviewee",	\[x] -> interviewee x	),
+
+
+	( "money",	\[x] -> money x	),
+	( "story",	\[x] -> story x	),
+	( "job",	\[x] -> job x	)
+	]
+
+story_inflections = [
+ ( "fragrance", "smell" ),
+ ( "lotion", "gift" ),
+ ( "visitor", "customer" ),
+ ( "visitors", "visitor" ),
+ ( "names", "name" ),
+ ( "cry", "cried" ),
+ ( "looked", "look" ),
+ ( "wear", "wore" ),
+ ( "visited", "visit" ),
+ ( "interviewed", "interview" ),
+ ( "interviewees", "interviewee" ),
+ ( "supervisors", "supervisor" ),
+ ( "taught", "teach" ),
+ ( "helped", "help" ),
+ ( "lost", "lose" ),
+ ( "volunteered", "volunteer" ),
+ ( "subjects", "subject" ),
+ ( "offended", "offend" ),
+ ( "angered", "anger"),
+ ( "greeted", "greet" )
+ ]
+
+story_relations = [
+	( "black",	\[x]	-> black x	),
+	( "white",	\[x]	-> white x	),
+	( "beautiful",	\[x]	-> beautiful x	),
+	( "homeless",	\[x]	-> homeless x	),
+	( "older",	\[x]	-> older x	),
+
+	( "dress",	\[x]	-> officewear x	),
+
+	( "cried",	\[x]	-> cried x	),
+
+	( "look",	\[x,y] -> look_at y x	),
+	( "lose",	\[x,y]	-> lose y x	),
+	( "help",	\[x,y]	-> help y x	),
+
+	( "volunteer",	\[x,y]	-> volunteer_at y x	),
+
+	( "wore",	\[x,y]	-> wore y x	),
+	( "interview",	\[x,y]	-> interview y x	)
+	]
+
