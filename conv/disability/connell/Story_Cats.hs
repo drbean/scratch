c7e66cd7820ee9f95f4b67cb7b715da6edf70370
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-02-27 10:15:06 +0000

    dic.yaml

--- conv/disability/connell/Story_Cats.hs
+++ conv/disability/connell/Story_Cats.hs
@@ -7,71 +7,40 @@ type WordConverted = ([String],String)
 multipart_names :: [WordConverted]
 
 multipart_names = [
-	(["the","state","of","colorado"],	"the_state_of_colorado"),
-	(["the","gathering","place"],	"the_gathering_place"),
-	(["administrative","assistant"],	"administrative_assistant"),
-	(["ten","dollar","bill"],	"ten_dollar_bill"),
-	(["birthday","card"],	"birthday_card")
+	(["troop","409"],	"troop_409")
 	]
 
 proper_names, object_names, story_verbs :: Lexset
 
 proper_names = [
-	[Cat "queen" "NP" [Thrd,Fem,Sg] []],
-	[Cat "ann"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "debra"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "tanya"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "jennifer"	"NP" [Thrd,Fem,Sg] []],
-	[Cat "the_state_of_colorado" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "the_gathering_place" "NP" [Thrd,Neutr,Sg] []],
-	[Cat "nobody_or_nothing" "NP" [Thrd,Neutr,Sg] []]
+	[Cat "derek" "NP" [Thrd,Masc,Sg] []],
+	[Cat "richard"	"NP" [Thrd,Masc,Sg] []],
+	[Cat "claudia"	"NP" [Thrd,Fem,Sg] []],
+	[Cat "randy"	"NP" [Thrd,Masc,Sg] []],
+	[Cat "troop_409" "NP" [Thrd,Masc,Sg] []],
+	[Cat "nobody_or_nothing_or_nowhere" "NP" [Thrd,Neutr,Sg] []]
 	]
 
 object_names = [
 
-	[Cat "money"	"NP" [Thrd,Neutr,Sg] [],
-		Cat "money"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "dress"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "dresses"	"CN" [Thrd,Neutr,Pl] []],
-
-	[Cat "rent"	"CN" [Thrd,Neutr,Sg] [],
-		Cat "rent"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "apartment"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "unemployment"	"NP" [Thrd,Neutr,Sg] []],
-	[Cat "ten_dollar_bill"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "birthday_card"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "sign"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "shelter"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "gift"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "lotion"	"NP" [Thrd,Neutr,Sg] [],
-		Cat "lotion"	"CN" [Thrd,Neutr,Pl] []],
-	[Cat "smell"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "spirits"	"CN" [Thrd,Neutr,Pl] []],
-
-
-
-	[Cat "administrative_assistant"	"NP" [Thrd,MascOrFem,Sg] []],
-	[Cat "donator"	"NP" [Thrd,Masc,Sg] []],
-	[Cat "supervisor"	"CN" [Thrd,Fem,Sg] []],
-	[Cat "supervisors"	"CN" [Thrd,Fem,Pl] []],
-	[Cat "interviewee"	"CN" [Thrd,MascOrFem,Sg] []],
-	[Cat "interviewees"	"CN" [Thrd,MascOrFem,Pl] []],
-	[Cat "visitor"	"CN" [Thrd,MascOrFem,Sg] []],
-	[Cat "visitors"	"CN" [Thrd,MascOrFem,Pl] []],
-	[Cat "home"	"CN" [Thrd,Neutr,Sg] []],
+	[Cat "scout"	"CN" [Thrd,Masc,Sg] []],
+	[Cat "scouts"	"CN" [Thrd,Masc,Pl] []],
+	[Cat "scoutmaster"	"CN" [Thrd,Masc,Sg] []],
+	[Cat "assistant_scoutmaster"	"CN" [Thrd,Fem,Sg] []],
+	[Cat "leader"	"CN" [Thrd,MascOrFem,Sg] []],
+	[Cat "leaders"	"CN" [Thrd,MascOrFem,Pl] []],
+	[Cat "troop"	"CN" [Thrd,Masc,Sg] [],
+		Cat "troop"	"CN" [Thrd,Masc,Pl] []],
 	[Cat "story"	"CN" [Thrd,Neutr,Sg] []],
 	[Cat "job"	"CN" [Thrd,Neutr,Sg] []],
 	[Cat "jobs"	"CN" [Thrd,Neutr,Pl] []],
 	[Cat "name"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "names"	"CN" [Thrd,Neutr,Pl] []],
-	[Cat "upbringing" "CN" [Thrd,Neutr,Sg] []]
+	[Cat "names"	"CN" [Thrd,Neutr,Pl] []]
 	]
 
 story_verbs = [
-	[Cat "cried"	"VP" [Tense] []],
-	[Cat "cry"	"VP" [Infl] []],
-	[Cat "lost"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "lose"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "led"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "led"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "helped"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "help"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "volunteered"	"VP" [Tense] [Cat "_" "PP" [At] []]],
@@ -82,32 +51,26 @@ story_verbs = [
 		Cat "taught"	"VP" [Tense] []],
 	[Cat "teach"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
 		Cat "teach"	"VP" [Infl] []],
-	[Cat "visited"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
-		Cat "visited"	"VP" [Tense] []],
-	[Cat "visit"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
-		Cat "visit"	"VP" [Infl] []],
-	[Cat "angered"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "anger"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "offended"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "offend"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "interviewed"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "interview"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "greeted"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "greet"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "wore"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "wear"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-	[Cat "wearing"	"GER" [Infl] [Cat "_" "NP" [AccOrDat] []]]
+	[Cat "greet"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]]
 	]
 
 story_aux = [
 	]
 
 story_adjs = [
+	[Cat "female"	"ADJ" [] []],
+	[Cat "male"	"ADJ" [] []],
 	[Cat "older"	"ADJ" [] []],
-	[Cat "homeless"	"ADJ" [] []],
-	[Cat "beautiful"	"ADJ" [] []],
-	[Cat "black"	"ADJ" [] []],
-	[Cat "white"	"ADJ" [] []]
+	[Cat "dedicated"	"ADJ" [] []],
+	[Cat "disabled"	"ADJ" [] []],
+	[Cat "physically-disabled"	"ADJ" [] []],
+	[Cat "mentally-disabled"	"ADJ" [] []],
+	[Cat "developmentally-disabled"	"ADJ" [] []],
+	[Cat "mentally-handicapped"	"ADJ" [] []],
+	[Cat "mentally-retarded"	"ADJ" [] []]
 	]
 
 
