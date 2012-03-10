e8e2ed06b6a86633914f173eff567e3b96de4104
drbean <drbean@users.noreply.github.com>
2012-03-10 05:43:48 +0000

    all parses. Second parse of 'Ellen sailed around the world in a boat' was needed. foldl takeWH, pickOne. firstparse. labelFormAnswers

--- csfp/Questioner.hs
+++ csfp/Questioner.hs
@@ -5,31 +5,45 @@ import System.IO
 import Parsing
 import LogicalForm
 
-firstparse :: [ParseTree Cat Cat] -> ParseTree Cat Cat
-firstparse [] = Ep
-firstparse (a:as) = a
-
 yesorno :: Answer -> String
 yesorno (Boolean False) = "No"
 yesorno (Boolean True)  = "Yes"
+
+takeWH :: String -> String -> String
+takeWH _ "WH" = "WH"
+takeWH "WH" _ = "WH"
+takeWH x _    = x
+
+pickOne :: String -> String -> String
+pickOne "NoAnswer" x   = x
+pickOne "[]" x         = x
+pickOne x   "NoAnswer" = x
+pickOne x       "[]"   = x
+pickOne "False" "True" = "True"
+pickOne "True" "False" = "True"
+pickOne "False" x      = x
+pickOne "True"  x      = x
+pickOne y       x      = x ++ ", " ++ y
 
 main = do
 	sentence <- getLine
 	let lexed = lexer sentence
 	putStrLn $ unwords lexed
-	let parse = firstparse. parses $ sentence
+	let parselist = parses sentence
 	hClose stderr
 	hDuplicateTo stdout stderr
-	let label = case (parse) of
-		Ep -> "Unparseable"
-		_  -> catLabel $ t2c parse
-	putStrLn $ label
-	let answer = case label of
-		"WH" -> transWH $ parse
-		_ -> transTXT $ Just parse
-	let response = case label of
-		"WH" -> show $ map ( toupper . named) $ evalW answer
-		"YN" -> yesorno $ eval answer
-		"S" -> show $ eval answer
-	putStrLn response
+	let labelFormAnswers =
+		map (\p -> (label p,form p,answer p)) parselist where
+			label Ep = "Unparseable"
+			label p  = catLabel $ t2c p
+			form p = case label p of
+				"WH" -> transWH $ p
+				_ -> transTXT (Just p)
+			answer p = case label p of
+				"WH" -> show $ map ( toupper . named) $
+						evalW $ form p
+				"YN" -> yesorno $ eval $ form p
+				"S" -> show $ eval $ form p
+	putStrLn $ foldl takeWH "S" $ map (\(l,f,a)->l) labelFormAnswers
+	putStrLn $ foldl pickOne "NoAnswer" $ map (\(l,f,a)->a) labelFormAnswers
 	hClose stdout
