e4077bd8da9759f5cac797c126f0914e51f3e91b
drbean <drbean@users.noreply.github.com>
2012-03-13 13:16:40 +0000

    cusp,aircraft,sail,feel_stress,etc

--- business/stress/adventure/Story_Interpretation.hs
+++ business/stress/adventure/Story_Interpretation.hs
@@ -6,24 +6,34 @@ story_objects = [
 	( "adventurer",	\[x]	-> adventurer x	),
 	( "teacher",	\[x]	-> teacher x	),
 
-	-- ( "control",	\[x] -> control x	),
-	-- ( "uncertainty",	\[x] -> uncertainty x	),
-	-- ( "support",	\[x] -> support x	),
-	( "pressure",	\[x] -> pressure x	)
+	( "lack_of_control",	\[x] -> lack_of_control x	),
+	( "uncertainty",	\[x] -> uncertainty x	),
+	( "lack_of_support",	\[x] -> lack_of_support x	),
+	( "pressure",	\[x] -> pressure x	),
+
+	( "world",	\[x] -> world x	),
+	( "plane",	\[x] -> plane x	),
+	( "boat",	\[x] -> boat x	),
+	( "glider",	\[x] -> glider x	),
+	( "balloon",	\[x] -> balloon x	)
 
 	]
 
 story_inflections = [
  ( "adventurers", "adventurer" ),
- ( "frameworks", "framework" )
+ ( "feel_stress", "felt_stress" ),
+ ( "sailed", "sail" ),
+ ( "flew", "fly" )
 
  ]
 
 story_relations = [
 	( "useful",	\[x]	-> useful x	),
 
+	( "felt_stress",	\args -> case args of [x] -> feel_stress x; [x,y] -> cause_stress y x	),
 	( "put_pressure",	\[x,y]	-> pressurize y x	),
-	( "sail",	\[x,y]	-> sail y x	),
+	( "sail",	\args -> case args of [x,y] -> sail_around y x; [x,y,z] -> sail_around_in z y x	),
+	( "fly",	\args -> case args of [x,y] -> fly_around y x; [x,y,z] -> fly_around_in z y x	),
 	( "anger",	\[x,y]	-> anger y x	)
 	]
 
