9c86f7bba09a8da242124c3ca3793b5f39f1e601
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-03-29 02:32:19 +0000

    print 0\n

---
+++ nlp/2/pa2-autocorrect-v1/python/test.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl 
+
+# Created: 03/29/2012 08:44:04 AM
+# Last Edit: 2012 Mar 29, 10:31:11 AM
+# $Id$
+
+=head1 NAME
+
+test.pl - output 0
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+test.pl -- prints "0"
+
+=cut
+
+print "0\n";
+
+exit;
+
+=head1 DESCRIPTION
+
+Is it possible to call perl program, use result.
+
+=cut
+
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of test.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
+
