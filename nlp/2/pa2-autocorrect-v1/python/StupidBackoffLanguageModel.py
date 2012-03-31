63cbcce4c70e9b391a2934e168fb61e2f9e5e718
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-03-31 12:40:01 +0000

    OOV,<unk>

--- nlp/2/pa2-autocorrect-v1/python/StupidBackoffLanguageModel.py
+++ nlp/2/pa2-autocorrect-v1/python/StupidBackoffLanguageModel.py
@@ -1,20 +1,58 @@
+import math, collections
+
 class StupidBackoffLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.bigramCounts = collections.defaultdict(lambda: collections.defaultdict(int))
+    self.unigramCounts = collections.defaultdict(lambda: 0)
+    self.unigramCounts['<unk>'] = 1;
+
+    self.totals = collections.defaultdict(lambda: 0)
+    self.total = 0
     self.train(corpus)
+    self.v = len(self.unigramCounts)
+    for key in self.unigramCounts.keys():
+      self.totals[key] = self.unigramCounts[key] + self.v
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      previous = '' 
+      for datum in sentence.data:  
+        token = datum.word
+        self.total = self.total + 1
+	if previous:
+          self.bigramCounts[previous][token] = self.bigramCounts[previous][token] + 1
+          self.unigramCounts[previous] = self.unigramCounts[previous] + 1
+	previous = token
+    self.v = len(self.unigramCounts)
+    for key in self.unigramCounts.keys():
+      self.totals[key] = self.unigramCounts[key] + self.v
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 9.0 
+    previous = '' 
+    for token in sentence:
+      if previous:
+        bicount = self.bigramCounts[previous][token]
+	unicount = self.unigramCounts[token]
+        if bicount > 0:
+          score += math.log(bicount)
+          score -= math.log(self.totals[previous])
+        else:
+          if unicount:
+            score += math.log(unicount)
+          else:
+            pass
+            # score += math.log(self.unigramCounts['<unk>'])
+          score -= math.log(self.total)
+      previous = token
+    return score
+
+# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
