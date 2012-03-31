2b63e799643987d5dbfb12c446c74c7a862ecc5b
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-03-31 07:08:38 +0000

    No improvement. math domain error

--- nlp/2/pa2-autocorrect-v1/python/LaplaceBigramLanguageModel.py
+++ nlp/2/pa2-autocorrect-v1/python/LaplaceBigramLanguageModel.py
@@ -1,20 +1,43 @@
+import math, collections
+
 class LaplaceBigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.bigramCounts = collections.defaultdict(lambda: collections.defaultdict(int))
+    self.unigramCounts = collections.defaultdict(lambda: 0)
+    self.totals = collections.defaultdict(lambda: 0)
     self.train(corpus)
+    self.v = len(self.unigramCounts)
+    for key in self.unigramCounts.keys():
+      self.totals[key] = self.unigramCounts[key] + self.v
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      previous = '' 
+      for datum in sentence.data:  
+        token = datum.word
+	if previous:
+          self.bigramCounts[previous][token] = self.bigramCounts[previous][token] + 1
+          self.unigramCounts[previous] = self.unigramCounts[previous] + 1
+	previous = token
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0 
+    previous = '' 
+    for token in sentence:
+      if previous:
+        count = self.bigramCounts[previous][token]
+        if count > 0:
+          score += math.log(count + 1)
+          score -= math.log(self.totals[previous])
+        else:
+          score -= math.log(self.totals[previous])
+      previous = token
+    return score
