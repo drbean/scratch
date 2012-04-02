6acee832bb5755eecd70f6b08b4dfcaee09a6d59
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-04-02 04:04:23 +0000

    local variable 'biscore' referenced before assignment

--- nlp/2/pa2-autocorrect-v1/python/CustomLanguageModel.py
+++ nlp/2/pa2-autocorrect-v1/python/CustomLanguageModel.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import math, collections
 
 class CustomLanguageModel:
@@ -6,10 +7,12 @@ class CustomLanguageModel:
     """Initialize your data structures in the constructor."""
     self.bigramCounts = collections.defaultdict(lambda: collections.defaultdict(int))
     self.reverseCounts = collections.defaultdict(lambda: collections.defaultdict(int))
+    self.unkbigramCounts = collections.defaultdict(lambda: collections.defaultdict(int))
+    self.unkreverseCounts = collections.defaultdict(lambda: collections.defaultdict(int))
     self.unigramCounts = collections.defaultdict(lambda: 0)
+    self.unkunigramCounts = collections.defaultdict(lambda: 0)
     self.nCounts = collections.defaultdict(lambda: 0)
     self.d = { 0: 0, 1: 0.5, 2: 0.75 }
-    self.totals = collections.defaultdict(lambda: 0)
     self.total = 0
     self.train(corpus)
 
@@ -18,34 +21,81 @@ class CustomLanguageModel:
         Compute any counts or other corpus statistics in this function.
     """  
     for sentence in corpus.corpus:
+      previous = '' 
       for datum in sentence.data:  
         token = datum.word
         self.total = self.total + 1
-        self.unigramCounts[token] = self.unigramCounts[token] + 1
+	if previous:
+          self.bigramCounts[previous][token] = self.bigramCounts[previous][token] + 1
+          self.reverseCounts[token][previous] = self.reverseCounts[token][previous] + 1
+          self.unigramCounts[previous] = self.unigramCounts[previous] + 1
+	previous = token
+    for sentence in corpus.corpus:
+      previous = '' 
+      for datum in sentence.data:  
+        token = datum.word
+        realtoken = token
+        self.unkbigramCounts['<unk>']['<unk>'] = 1
+        self.unkunigramCounts['<unk>'] = 1
+	if previous:
+          if self.unigramCounts[ previous ] == 1:
+            previous = '<unk>'
+          if self.unigramCounts[ token ] == 1:
+            token = '<unk>'
+          self.unkbigramCounts[previous][token] = self.unkbigramCounts[previous][token] + 1
+          self.unkreverseCounts[token][previous] = self.unkreverseCounts[token][previous] + 1
+          self.unkunigramCounts[previous] = self.unkunigramCounts[previous] + 1
+	previous = realtoken
     self.v = len(self.unigramCounts)
     self.newtotal = self.total + self.v
     for key in self.unigramCounts.keys():
-      self.totals[key] = self.unigramCounts[key]
       self.n = self.unigramCounts[key]
       self.nCounts[self.n] = self.nCounts[ self.n ] + 1
+    self.totalreverse=0
+    self.totalunkreverse=0
+    for thisword in self.bigramCounts.keys():
+      for previousword in self.bigramCounts[thisword]:
+        self.totalreverse += self.bigramCounts[thisword][previousword]
+    for thisword in self.unkbigramCounts.keys():
+      for previousword in self.unkbigramCounts[thisword]:
+        self.totalunkreverse += self.unkbigramCounts[thisword][previousword]
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    factor = 20.0 
+    d = 0.75
+    score = 0
+    biscore = 0
+    previous = '' 
     for token in sentence:
-      unicount = self.unigramCounts[token]
-      ncount = self.nCounts[ unicount ]
-      it = ncount + 1
-      if self.nCounts.has_key(it):
-        ncountP = self.nCounts[it]
-      else:
-        ncountP = 0.9 * ncount
-      if unicount:
-        score = factor * ( unicount + 1 ) * (ncountP) / ( ncount )
-      else:
-        score = factor * ( self.nCounts[ 1 ]) / (self.total)
+      if previous:
+        bicount = self.bigramCounts[previous][token]
+	unicount = self.unigramCounts[previous]
+        reversecount = len(self.reverseCounts[token])
+        unkbicount = self.unkbigramCounts[previous][token]
+	unkunicount = self.unkunigramCounts[token]
+        unkreversecount = len(self.unkreverseCounts[token])
+        if bicount > 0 and unicount > 0:
+          biscore = bicount / unicount
+          continuation = reversecount / self.totalreverse
+        else:
+          if not self.unigramCounts.has_key(previous):
+            previous = '<unk>'
+            unkbicount = self.unkbigramCounts[previous][token]
+            biscore = unkbicount / self.unkunigramCounts[previous]
+          elif not self.unigramCounts.has_key(token):
+            token = '<unk>'
+            unkbicount = self.unkbigramCounts[previous][token]
+            biscore = unkbicount / self.unkunigramCounts[previous]
+          else:
+            unkbicount = self.unkbigramCounts['<unk>']['<unk>']
+          if previous == '<unk>' or token == '<unk>':
+            continuation = unkreversecount / self.totalunkreverse
+          else:
+            continuation = reversecount / self.totalreverse
+        score += math.log(biscore + continuation)
+      previous = token
     return score
 
 # vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
