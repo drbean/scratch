3305622c7184c3a9f92916b5391542f1416bc384
drbean <drbean@users.noreply.github.com>
2012-06-07 09:58:16 +0000

    To 2008, Fall Semester, mostly pre-NUU. Some NUU updates in June, 2012.

---
+++ job/spam.sh
@@ -0,0 +1,38 @@
+#!/usr/pkg/bin/bash
+
+# #!/bin/sh
+
+subject="Job application by Greg Matheson";
+
+#for address in \
+#	"Greg_Matheson <gregoer@healthy..chinmin.tw>" \
+#	"Greg Matheson <greg@healthy.chinmin.edu.tw>" \
+#	"Greg J Matheson <greg@healthy.chinmin.edu.tw>" \
+#	"Greg John Matheson <greg@healthy.chinmin.edu.tw>" \
+#	;
+#do
+#	n=0;
+#	for part in $address;
+#	do
+#		addressarray[$((n++))]=$part;
+#	done;
+
+while read -a addressarray;
+do
+	last=${#addressarray[@]};
+	addresspart=${addressarray[$((--last))]};
+	unset addressarray[$last];
+	display=${addressarray[*]};
+	sed -e "1i\\
+To: $display $addresspart
+	" -e "1i\\
+From: Greg Matheson <drbean@freeshell.org>
+	" -e "1i\\
+Subject: $subject
+	" < ShortUniversityCoverLetter | /usr/sbin/sendmail -oem -oi $addresspart;
+
+	error=$?;
+	echo -n $display $addresspart;
+	if [[ $error == 0 ]]; then echo " OK"; else echo " NOK"; fi;
+	sleep 45;
+done
