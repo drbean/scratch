a5b87f29ded1920037c1f8b66dbf20177a49818e
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-07-02 00:23:12 +0000

    mutt -s "Job application by Greg Matheson" -e "my_hdr To: Eagle 23418 <352b6c48@ads.tealit.com>" -H letter_bushiban -a ../html/P6270001.JPG -a ../html/passport.jpg -a ../html/diploma.jpg  -- 352b6c48@ads.tealit.com

---
+++ job/spam_mutt.sh
@@ -0,0 +1,40 @@
+#!/usr/pkg/bin/bash
+
+# #!/bin/sh
+
+subject="Job application by Greg Matheson";
+
+#for address in \
+#	"Greg_Matheson <gregoer@healthy..chinmin.tw>" \
+#	"Greg Matheson <greg@healthy.chinmin.edu.tw>" \
+#	"Greg J Matheson <greg@healthy.chinmin.edu.tw>" \
+#	"Greg John Matheson <greg@healthy.chinmin.edu.tw>" \
+#	;
+#do
+#	n=0;
+#	for part in $address;
+#	do
+#		addressarray[$((n++))]=$part;
+#	done;
+
+while read -a addressarray;
+do
+	last=${#addressarray[@]};
+	addresspart=${addressarray[$((--last))]};
+	unset addressarray[$last];
+	display=${addressarray[*]};
+	sed -e "1i\\
+To: $display $addresspart
+	" -e "1i\\
+From: Greg Matheson <drbean@freeshell.org>
+	" -e "1i\\
+Subject: $subject
+	" -e "1i\\
+Content-Type: text/plain; charset=\"UTF-8\"
+	" < letter_bushiban | /usr/sbin/sendmail -oem -oi $addresspart;
+
+	error=$?;
+	echo -n $display $addresspart;
+	if [[ $error == 0 ]]; then echo " OK"; else echo " NOK"; fi;
+	sleep 35;
+done
