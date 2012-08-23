a904b2eaac8c9c65e401c8c9798ffd0912acca0e
drbean <drbean@users.noreply.github.com>
2012-08-23 13:45:24 +0000

    conversation work Topic becomes business Area

---
+++ business/Topic_Interpretation.hs
@@ -0,0 +1,25 @@
+module Topic_Interpretation where 
+
+import Data.List
+
+import Model
+-- import Story_Interpretation
+
+objects, relations :: [( String, [Entity] -> Bool)]
+objects = [
+	( "worker",	\ [x] -> worker x	),
+	( "administrative_assistant",	\ [x] -> secretary x	),
+	( "daughter",	\ [x] -> daughter x	),
+	( "mother",	\ [x] -> isMother x	)
+ ]
+
+inflections :: [(String, String)]
+inflections = [
+ ]
+
+relations = [
+	( "disappointed",	\[x]	-> disappointed x	)
+ 
+ ]
+
+-- vim: set ts=8 sts=4 sw=4 noet:
