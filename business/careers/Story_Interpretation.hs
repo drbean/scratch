647d7136e7db498e5514bcfca47875cb29a6362f
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-08-26 11:12:56 +0000

    Fast-Track,Barbara,Tadeusz,Eva

--- business/careers/Story_Interpretation.hs
+++ business/careers/Story_Interpretation.hs
@@ -2,26 +2,17 @@ module Story_Interpretation where
 
 import Model
 
-story_objects = [
+objects = [
 	( "boss",	\[x] -> boss x	),
 	( "company",	\[x] -> company x	),
-	( "interviewee",	\[x] -> interviewee x	),
-	( "visitor",	\[x] -> visitor x	),
-	( "school",	\[x] -> school x	),
+	( "candidate",	\[x] -> candidate x	),
 	( "subject",	\[x] -> subject x	),
-	( "comment",	\[x] -> language x	),
-	( "request",	\[x] -> request x	),
-	( "treatment",	\[x] -> treatment x	),
-	( "upbringing",	\[x] -> upbringing x	),
 
 
-	( "money",	\[x] -> money x	),
-	( "story",	\[x] -> story x	),
-	( "dress",	\[x] -> dress x	),
 	( "job",	\[x] -> job x	)
 	]
 
-story_inflections = [
+inflections = [
  ( "supervisor", "boss" ),
  ( "information_technology", "i_t" ),
  ( "visitor", "customer" ),
@@ -29,33 +20,39 @@ story_inflections = [
  ( "comments", "comment" ),
  ( "requests", "request" ),
  ( "names", "name" ),
- ( "looked", "look" ),
- ( "wear", "wore" ),
+ -- ( "appeared", "appear" ),
  ( "visited", "visit" ),
  ( "interviewed", "interview" ),
  ( "interviewees", "interviewee" ),
  ( "taught", "teach" ),
  ( "subjects", "subject" ),
- ( "offended", "offend" ),
- ( "angered", "anger"),
  ( "greeted", "greet" )
  ]
 
-story_relations = [
-	( "red",	\[x]	-> red x	),
-	( "offensive",	\[x]	-> offensive x	),
-	( "mean",	\[x]	-> mean x	),
-	( "unfair",	\[x]	-> unfair x	),
+relations = [
+	( "thirty",	\[x] -> thirty x	),
+	( "fifty-two",	\[x] -> fifty_two x	),
+	( "forty-two",	\[x] -> forty_two x	),
 
-	( "look",	\[x,y] -> look_at y x	),
-	( "wore",	\[x,y]	-> wore y x	),
+	( "energetic",	\[x] -> energetic x	),
+	( "confident",	\[x] -> confident x	),
+	( "aggressive",	\[x] -> aggressive x	),
+
+	( "calm",	\[x] -> calm x	),
+	( "relaxed",	\[x] -> relaxed x	),
+	( "hard-working",	\[x] -> hard_working x	),
+	( "practical",	\[x] -> practical x	),
+	( "reliable",	\[x] -> reliable x	),
+
+	( "quiet",	\[x] -> quiet x	),
+	( "nervous",	\[x] -> nervous x	),
+
+	-- ( "appear",	\[x,y] -> appear y x	),
 	( "visit",	\[x,y]	-> visit y x	),
 	( "interview",	\[x,y]	-> interview y x	),
 	( "teach",	\args -> case args of
 		[x,y] -> (teach_who y x || teach_what y x)
 		[x,y,z] -> teach z y x ),
-	( "offend",	\[x,y]	-> offend y x	),
-	( "anger",	\[x,y]	-> anger y x	),
 	( "greet",	\[x,y]	-> greet y x	)
 	]
 
