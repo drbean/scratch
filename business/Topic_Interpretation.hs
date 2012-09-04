3d22ed42d29ac534cee6650547cde29802094f05
drbean <drbean@users.noreply.github.com>
2012-09-04 03:05:52 +0000

    worker,job,help.volunteer

--- business/Topic_Interpretation.hs
+++ business/Topic_Interpretation.hs
@@ -8,15 +8,22 @@ import Model
 objects, relations :: [( String, [Entity] -> Bool)]
 objects = [
 	( "administrative_assistant",   \ [x] -> secretary x    ),
-	( "worker",	\ [x] -> worker x	)
+	( "worker",	\ [x] -> worker x	),
+	( "job",	\[x]	-> job x	)
  ]
 
 inflections :: [(String, String)]
 inflections = [
+ ( "helped", "help" ),
+ ( "lost", "lose" ),
+ ( "volunteered", "volunteer" )
  ]
 
 relations = [
-	( "job",	\[x]	-> job x	)
+	( "help",	\[x,y]	-> help y x	),
+
+	( "volunteer",	\[x]	-> volunteer x	),
+	( "volunteer",	\[x,y]	-> volunteer_at y x	)
  
  ]
 
