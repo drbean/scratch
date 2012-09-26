27d108400fcff9001fc0d293bd353879ab531479
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-09-26 06:41:43 +0000

    supporting characters, sales vocab

--- business/careers/Story_Interpretation.hs
+++ business/careers/Story_Interpretation.hs
@@ -3,19 +3,39 @@ module Story_Interpretation where
 import Model
 
 objects = [
+	( "polish",	\[x] -> polish x	),
+	( "german",	\[x] -> german x	),
+	( "american",	\[x] -> american x	),
 	( "boss",	\[x] -> boss x	),
 	( "company",	\[x] -> company x	),
+	( "interviewer",	\[x] -> interviewer x	),
+	( "interviewee",	\[x] -> interviewee x	),
 	( "candidate",	\[x] -> candidate x	),
 	( "co-worker",	\[x] -> co_worker x	),
+	( "team_member",	\[x] -> team_member x	),
+	( "sales_representative",	\[x] -> sales_representative x	),
+	( "sales_manager",	\[x] -> sales_manager x	),
+	( "sales_experience",	\[x] -> sales_experience x	),
 	( "subject",	\[x] -> subject x	),
+	( "personality",	\[x] -> personality x	),
+	( "ideas",	\[x] -> ideas x	),
+	( "presentation",	\[x] -> story x	),
 
 
 	( "job",	\[x] -> job x	)
 	]
 
 inflections = [
+ ( "thirty_years_old", "thirty" ),
+ ( "fifty-two_years_old", "fifty-two" ),
+ ( "forty-two_years_old", "forty-two" ),
  ( "supervisor", "boss" ),
+ ( "bosses", "boss" ),
+ ( "team_player", "team_member" ),
+ ( "team-player", "team_member" ),
+ ( "jobs", "job" ),
  ( "co-workers", "co-worker" ),
+ ( "candidates", "candidate" ),
  ( "information_technology", "i_t" ),
  ( "comments", "comment" ),
  ( "requests", "request" ),
@@ -35,9 +55,13 @@ relations = [
 	( "fifty-two",	\[x] -> fifty_two x	),
 	( "forty-two",	\[x] -> forty_two x	),
 
+	( "successful",	\[x] -> successful x	),
+
 	( "energetic",	\[x] -> energetic x	),
 	( "confident",	\[x] -> confident x	),
 	( "aggressive",	\[x] -> aggressive x	),
+	( "ambitious",	\[x] -> ambitious x	),
+	( "strong",	\[x] -> strong x	),
 
 	( "calm",	\[x] -> calm x	),
 	( "relaxed",	\[x] -> relaxed x	),
