9f0a65c357732d8c2ae4ca057d9082bda04ea698
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-11-02 14:08:24 +0000

    speak,talk,know,asked,said,told

--- business/Topic_Interpretation.hs
+++ business/Topic_Interpretation.hs
@@ -13,12 +13,22 @@ objects = [
 
 inflections :: [(String, String)]
 inflections = [
+ ( "speak", "talk" ),
  ( "job", "role" ),
  ( "lost", "lose" )
  ]
 
 relations = [
 
+	( "know", \[x,y] -> know y x ),
+	( "talk",	\args -> case args of 
+		[x,y] -> talk_with_or_about y x
+		[x,y,z] -> talk_with_about z y x	),
+	( "asked", \args -> case args of
+	    [x,y] -> asked y x
+	    [x,y,z] -> ask_about z y x ),
+	( "said", \[x,y] -> said y x ),
+	( "told", \args -> case args of [x,y] -> recite y x; [x,y,z] -> told z y x )
  
  ]
 
