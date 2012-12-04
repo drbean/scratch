0be75eb1d987275bb735c9a8a895057522c4f91d
drbean <drbean@users.noreply.github.com>
2012-12-04 10:55:48 +0000

    decided_to_charge,offered_to_buy,entitites,pred1

--- business/greatideas/Story_Interpretation.hs
+++ business/greatideas/Story_Interpretation.hs
@@ -19,15 +19,13 @@ objects = [
 	-- ( "personality",	\[x] -> personality x	),
 	-- ( "ideas",	\[x] -> ideas x	),
 	-- ( "presentation",	\[x] -> story x	),
-	( "website",	\[x] -> website x	),
-	( "website_designer",	\[x] -> website_designer x	),
-	( "manager",	\[x] -> manager x	)
+	( "website",	\[x] -> website x	)
 
 
 	]
 
 inflections = [
- ( "want_to_charge", "wanted_to_charge" ),
+ ( "decide_to_charge", "decided_to_charge" ),
  ( "want_to_have", "wanted_to_have" ),
  ( "want_to_finish", "wanted_to_finish" ),
  ( "want_to_pay", "wanted_to_pay" ),
@@ -36,45 +34,15 @@ inflections = [
  ]
 
 relations = [
-	-- ( "thirty",	\[x] -> thirty x	),
-	-- ( "fifty-two",	\[x] -> fifty_two x	),
-	-- ( "forty-two",	\[x] -> forty_two x	),
-
-	-- ( "successful",	\[x] -> successful x	),
-	-- ( "good",	\[x] -> good x	),
-
-	-- ( "energetic",	\[x] -> energetic x	),
-	-- ( "confident",	\[x] -> confident x	),
-	-- ( "aggressive",	\[x] -> aggressive x	),
-	-- ( "ambitious",	\[x] -> ambitious x	),
-	-- ( "strong",	\[x] -> strong x	),
-
-	-- ( "calm",	\[x] -> calm x	),
-	-- ( "relaxed",	\[x] -> relaxed x	),
-	-- ( "hard-working",	\[x] -> hard_working x	),
-	-- ( "practical",	\[x] -> practical x	),
-	-- ( "reliable",	\[x] -> reliable x	),
-
-	-- ( "quiet",	\[x] -> quiet x	),
-
-	( "finish",	\[x,y,z] -> finish z y x	),
-	( "pay",	\args -> case args of
+	( "finish",	\[x,y,z] -> finish z y x	)
+	, ( "pay",	\args -> case args of
 		[x,y,z] -> pay z y x
-		[x,y] -> forgetful  pay y x ),
+		[x,y] -> forgetful  pay y x )
+	, ( "offered_to_pay", \[x,y,z] -> offered_to_pay z y x )
 
-	( "wanted_charge",	\args -> case args of
-		[x,y,z] -> wanted_to_charge_sum z y x
-		[x,y,z,w] -> wanted_to_charge w z y x	),
-	( "wanted_to_have",	\[x,y,z] -> wanted_to_have z y x	),
-
-	( "wanted_finish",	\args -> case args of
-		-- [x,y,z] -> wanted_to_finish z y x
-		[x,y,z,w] -> wanted_to_finish w z y x	),
-
-	-- ( "wanted",	\[x] -> wanted x	),
-	( "wanted_to_pay",	\args -> case args of 
-		[x,y,z] -> wanted_to_pay z y x
-		[x,y,z,w] -> wanted_to_pay_sum w z y x	)
+	, ( "decided_to_charge",	\args -> case args of
+		[x,y,z] -> decided_to_charge z y x
+		[x,y,z,w] -> decided_to_charge_entry w z y x	)
 
 	-- -- ( "appear",	\[x,y] -> appear y x	),
 	-- ( "help",	\[x,y]	-> help y x	),
