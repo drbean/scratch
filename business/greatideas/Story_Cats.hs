73784a102d7f81768ecf91e52dd01496bdb9328b
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-12-15 04:36:11 +0000

    cars:pay_to_enter,decided_to_charge,started_to_charge,wanted_to_look,wanted_to_pay,have_to_pay,intransit,forgetful,visitors,cars,ten_dollars,child,experiment,unsuccessful

--- business/greatideas/Story_Cats.hs
+++ business/greatideas/Story_Cats.hs
@@ -5,79 +5,49 @@ import Parsing
 names, nouns, verbs, aux, adjs, advs :: Lexset
 
 names = [
-	[Cat "penske-wynn" "NP" [Thrd,Neutr,Sg] []]
+	[Cat "steve_wynn" "NP" [Thrd,Masc,Sg] []]
 	, [Cat "the_ferrari_showroom" "NP" [Thrd,Neutr,Sg] []]
-	, [Cat "ten_dollars" "NP" [Thrd,Neutr,Sg] []]
-	, [Cat "pepsi" "NP" [Thrd,Neutr,Sg] []]
-	, [Cat "punjabi_farmers" "NP" [Thrd,MascOrFem,Pl] []]
-	, [Cat "the_punjabi_government"	"NP" [Thrd,Neutr,Sg] []]
-	, [Cat "alex_tew"	"NP" [Thrd,Masc,Sg] []]
-	, [Cat "the_million_dollar_homepage"	"NP" [Thrd,Neutr,Sg] []]
-	, [Cat "facebook"	"NP" [Thrd,Neutr,Sg] []]
-	, [Cat "mark_zuckerberg"	"NP" [Thrd,Masc,Sg] []]
+	-- , [Cat "pepsi" "NP" [Thrd,Neutr,Sg] []]
+	-- , [Cat "punjabi_farmers" "NP" [Thrd,MascOrFem,Pl] []]
+	-- , [Cat "the_punjabi_government"	"NP" [Thrd,Neutr,Sg] []]
+	-- , [Cat "alex_tew"	"NP" [Thrd,Masc,Sg] []]
+	-- , [Cat "the_million_dollar_homepage"	"NP" [Thrd,Neutr,Sg] []]
+	-- , [Cat "facebook"	"NP" [Thrd,Neutr,Sg] []]
+	-- , [Cat "mark_zuckerberg"	"NP" [Thrd,Masc,Sg] []]
 	]
 
 nouns = [
-
-	-- [Cat "area"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "time"	"CN" [Thrd,Neutr,Sg] [],
-	-- 	Cat "time"	"NP" [Thrd,Neutr,Sg] []],
-	-- [Cat "dollars"	"CN" [Thrd,Neutr,Pl] []],
-	-- [Cat "money"	"CN" [Thrd,Neutr,Sg] [],
-	-- 	Cat "money"	"NP" [Thrd,Neutr,Sg] []],
-	-- [Cat "payment"	"CN" [Thrd,Neutr,Sg] [],
-	-- 	Cat "payment"	"NP" [Thrd,Neutr,Sg] []],
-	-- [Cat "design"	"CN" [Thrd,Neutr,Sg] [],
-	-- 	Cat "design"	"NP" [Thrd,Neutr,Sg] []],
-	-- [Cat "deal"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "bookstore"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "image"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "images"	"CN" [Thrd,Neutr,Pl] []],
-	[Cat "oranges"	"CN" [Thrd,Neutr,Pl] []],
-	[Cat "website"	"CN" [Thrd,Neutr,Sg] []],
-	[Cat "site"	"CN" [Thrd,Neutr,Sg] []]
-	-- [Cat "size"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "amount"	"CN" [Thrd,Neutr,Sg] []],
-	-- [Cat "information"	"NP" [Thrd,Neutr,Sg] []],
-
-	-- [Cat "agreement"	"NP" [Thrd,Neutr,Sg] []],
-
-	-- [Cat "manager"	"CN" [Thrd,MascOrFem,Sg] []],
-	-- [Cat "website_designer"	"CN" [Thrd,MascOrFem,Sg] []]
+	[Cat "visitor"	"CN" [Thrd,MascOrFem,Sg] []]
+	, [Cat "visitors"	"CN" [Thrd,MascOrFem,Pl] []]
+	, [Cat "car" "CN" [Thrd,Neutr,Sg] []]
+	, [Cat "cars" "CN" [Thrd,Neutr,Pl] []]
+	, [Cat "ten_dollars" "NP" [Thrd,Neutr,Sg] []]
+	, [Cat "entrance_fee" "CN" [Thrd,Neutr,Sg] []]
+	, [Cat "experiment" "CN" [Thrd,Neutr,Sg] []]
+	
+	-- [Cat "oranges"	"CN" [Thrd,Neutr,Pl] []],
+	-- [Cat "website"	"CN" [Thrd,Neutr,Sg] []],
+	-- [Cat "site"	"CN" [Thrd,Neutr,Sg] []]
 	]
 
 verbs = [
-	[Cat "offered" "VP" [Tense] [Cat "_" "INF" [To] []]],
-	[Cat "offer" "VP" [Infl] [Cat "_" "INF" [To] []]],
-	[Cat "decided" "VP" [Tense] [Cat "_" "INF" [To] []]],
-	[Cat "decide" "VP" [Infl] [Cat "_" "INF" [To] []]],
+	-- [Cat "offered" "VP" [Tense] [Cat "_" "INF" [To] []]],
+	-- [Cat "offer" "VP" [Infl] [Cat "_" "INF" [To] []]],
+	[Cat "started" "VP" [Tense] [Cat "_" "INF" [To] []]],
+	[Cat "start" "VP" [Infl] [Cat "_" "INF" [To] []]],
+	[Cat "looked" "VP" [Tense] [Cat "_" "PP" [At] []]],
+	[Cat "look" "VP" [Infl] [Cat "_" "PP" [At] []]],
+	[Cat "entered" "VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "enter" "VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "buy"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
 		Cat "buy"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
 						Cat "_" "PP" [From] []]],
-	-- [Cat "liked" "VP" [Tense] [Cat "_" "NP" [AccOrDat]       []]],
-	-- [Cat "like" "VP" [Infl] [Cat "_" "NP" [AccOrDat]       []]],
-	-- [Cat "accepted" "VP" [Tense] [Cat "_" "NP" [AccOrDat]       []]],
-	-- [Cat "accept" "VP" [Infl] [Cat "_" "NP" [AccOrDat]       []]],
-	-- [Cat "agreed" "VP" [Tense] [Cat "_" "INF" [To]       []]],
-	-- [Cat "agree" "VP" [Infl] [Cat "_" "INF" [To]       []]],
-        -- [Cat "compromised" "VP" [Tense] [Cat "_" "PP" [On]       []]],
-        -- [Cat "compromise" "VP" [Infl] [Cat "_" "PP" [On]       []]],
-        -- [Cat "came" "VP" [Tense] [Cat "_" "PP" [To]       []]],
-        -- [Cat "come" "VP" [Infl] [Cat "_" "PP" [To]       []]],
-        -- [Cat "operate" "VP" [Infl] [Cat "_" "PP" [In]       []]],
-	-- [Cat "show"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
-	-- 	Cat "show"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
-	-- 					Cat "_" "PP" [On] []]],
-	-- [Cat "put"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
-	-- 				Cat "_" "PP" [On] []]],
-	-- [Cat "take"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
-	-- 	Cat "take"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
-	-- 					Cat "_" "INF" [To] []]],
-	[Cat "finish"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
-					Cat "_" "PP" [In] []]],
-	-- [Cat "limit"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
-
-	[Cat "pay"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
+	[Cat "paid"	"VP" [Tense] [],
+		Cat "paid"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "paid" "VP" [Tense]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "NP" [AccOrDat] []]],
+	[Cat "pay"	"VP" [Infl] [],
+		Cat "pay"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
 		Cat "pay" "VP" [Infl]  [Cat "_" "NP" [AccOrDat] [],
                                                 Cat "_" "NP" [AccOrDat] []]],
         [Cat "charge" "VP" [Infl]  [Cat "_" "NP" [AccOrDat] []],
@@ -94,6 +64,8 @@ adjs = [
 	-- [Cat "lots_of"	"ADJ" [] []],
 	-- [Cat "a_lot_of"	"ADJ" [] []],
 	-- [Cat "popular"	"ADJ" [] []],
+	[Cat "successful"	"ADJ" [] []]
+	, [Cat "unsuccessful"	"ADJ" [] []]
 	-- [Cat "good"	"ADJ" [] []]
 	]
 
--- business/greatideas/Story_Interpretation.hs
+++ business/greatideas/Story_Interpretation.hs
@@ -3,52 +3,77 @@ module Story_Interpretation where
 import Model
 
 objects = [
-	-- ( "polish",	\[x] -> polish x	),
-	-- ( "german",	\[x] -> german x	),
-	-- ( "american",	\[x] -> american x	),
-	-- ( "boss",	\[x] -> boss x	),
-	-- ( "company",	\[x] -> company x	),
-	-- ( "interviewer",	\[x] -> interviewer x	),
-	-- ( "interviewee",	\[x] -> interviewee x	),
-	-- ( "candidate",	\[x] -> candidate x	),
-	-- ( "co-worker",	\[x] -> co_worker x	),
-	-- ( "team_member",	\[x] -> team_member x	),
-	-- ( "sales_representative",	\[x] -> sales_representative x	),
-	-- ( "sales_experience",	\[x] -> sales_experience x	),
-	-- ( "subject",	\[x] -> subject x	),
-	-- ( "personality",	\[x] -> personality x	),
-	-- ( "ideas",	\[x] -> ideas x	),
-	-- ( "presentation",	\[x] -> story x	),
-	( "oranges",	\[x] -> oranges x	),
-	( "website",	\[x] -> website x	)
+	-- ( "oranges",	\[x] -> oranges x	)
+	-- , ( "website",	\[x] -> website x	)
+	 ( "entrance_fee",	\[x] -> entrance_fee x	)
+	 , ( "visitors",	\[x] -> visitors x	)
+	 , ( "cars",	\[x] -> cars x	)
+	 , ( "ten_dollars",	\[x] -> ten_dollars x	)
+	 , ( "child",	\[x] -> child x	)
+	 , ( "experiment",	\[x] -> experiment x	)
 
 
 	]
 
 inflections = [
- ( "site", "website" ),
+ -- ( "site", "website" ),
+ ( "visitor",	"visitors" ),
+ ( "car", "cars" ),
+ ( "entrance_fee", "ten_dollars" ),
+ ( "children", "child" ),
  ( "decide_to_charge", "decided_to_charge" ),
+ ( "started_to_charge", "decided_to_charge" ),
+ ( "start_to_charge",  "decided_to_charge" ),
  ( "offer_to_buy", "offered_to_buy" ),
+ ( "bought", "buy" ),
+ ( "want_to_pay", "wanted_to_pay" ),
+ ( "want_to_buy", "wanted_to_buy" ),
+ ( "want_to_look", "wanted_to_look" ),
  ( "want_to_have", "wanted_to_have" ),
  ( "want_to_finish", "wanted_to_finish" ),
- ( "want_to_pay", "wanted_to_pay" ),
+ ( "want_to_finish", "wanted_to_finish" ),
+ ( "paid", "pay" ),
+ ( "entered", "enter" ),
+ ( "paid_to_enter", "pay_to_enter" ),
  ( "want_to_pay_sum", "wanted_to_pay_sum" ),
  ( "want", "wanted" )
  ]
 
 relations = [
-	( "finish",	\[x,y,z] -> finish z y x	)
+	( "successful",	\[x] -> successful x	)
+	, ( "unsuccessful",	\[x] -> unsuccessful x	)
+	, ( "finish",	\[x,y,z] -> finish z y x	)
 	, ( "pay",	\args -> case args of
 		[x,y,z] -> pay z y x
-		[x,y] -> forgetful  pay y x )
-	, ( "offered_to_buy", \args -> case args of 
-		[x,y,z] -> offered_to_buy z y x
-		[x,y,z,w] -> offered_to_buy_from w z y x )
-	, ( "offered_to_pay", \[x,y,z] -> offered_to_pay z y x )
+		[x,y] -> forgetful  pay y x
+		[x] -> (intransit . forgetful)  pay x )
+	, ( "enter",	\args -> case args of
+		[x,y] -> enter y x )
+	, ( "pay_to_enter",	\args -> case args of
+		[x,y,z] -> pay_to_enter z y x )
+	, ( "have_to_pay",	\args -> case args of
+		[x,y,z] -> have_to_pay z y x )
 
+	--, ( "offered_to_buy", \args -> case args of 
+	--	[x,y,z] -> offered_to_buy z y x
+	--	[x,y,z,w] -> offered_to_buy_from w z y x )
+	--, ( "offered_to_pay", \[x,y,z] -> offered_to_pay z y x )
+
+	, ( "buy", \args -> case args of 
+		[x,y] -> buy y x )
+
+	, ( "wanted_to_look", \args -> case args of 
+		[x,y] -> wanted_to_look y x
+		[x,y,z] -> wanted_to_look_at z y x )
+	, ( "wanted_to_pay", \args -> case args of 
+		[x,y,z] -> wanted_to_pay z y x )
+	, ( "wanted_to_buy", \args -> case args of 
+		[x,y] -> wanted_to_buy y x
+		[x,y,z] -> wanted_to_buy_some z y x )
 	, ( "decided_to_charge",	\args -> case args of
 		[x,y,z] -> decided_to_charge z y x
-		[x,y,z,w] -> decided_to_charge_entry w z y x	)
+		[x,y,z,w] -> decided_to_charge_fee w z y x
+		[x,y,z,w,u] -> decided_to_charge_entry u w z y x	)
 
 	-- -- ( "appear",	\[x,y] -> appear y x	),
 	-- ( "help",	\[x,y]	-> help y x	),
