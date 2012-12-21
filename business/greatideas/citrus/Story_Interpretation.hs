c720ac5ef190b9d1e750fef9573530fb468c5d29
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-12-21 12:56:57 +0000

    a_good_price,helped,paid,pay,sold,grew,help_to_make,wanted_to_help

--- business/greatideas/citrus/Story_Interpretation.hs
+++ business/greatideas/citrus/Story_Interpretation.hs
@@ -8,6 +8,7 @@ objects = [
 	 ,( "land",	\[x] -> land x	)
 	 , ( "experiment",	\[x] -> experiment x	)
 	 , ( "good_idea",	\[x] -> good_idea x	)
+	 , ( "a_good_price",	\[x] -> a_good_price x	)
 
 
 	]
@@ -16,8 +17,13 @@ inflections = [
  ( "offer_to_buy", "offered_to_buy" ),
  ( "bought", "buy" ),
  ( "want_to_pay", "wanted_to_pay" ),
+ ( "sold", "sell" ),
  ( "want_to_sell", "wanted_to_sell" ),
+ ( "grew", "grow" ),
  ( "want_to_grow", "wanted_to_grow" ),
+ ( "helped", "help" ),
+ ( "want_to_help", "wanted_to_help" ),
+ ( "help_to_make", "helped_to_make" ),
  ( "want_to_buy", "wanted_to_buy" ),
  ( "want_to_look", "wanted_to_look" ),
  ( "want_to_have", "wanted_to_have" ),
@@ -30,9 +36,10 @@ relations = [
 	, ( "unsuccessful",	\[x] -> unsuccessful x	)
 	, ( "finish",	\[x,y,z] -> finish z y x	)
 	, ( "pay",	\args -> case args of
-		[x,y,z] -> pay z y x
-		[x,y] -> forgetful  pay y x
-		[x] -> (intransit . forgetful)  pay x )
+		[x,y,z,w] -> pay w z y x
+		[x,y,z] -> unditrans pay z y x
+		[x,y] -> (forgetful . unditrans) pay y x
+		[x] -> (intransit . forgetful . unditrans)  pay x )
 	, ( "have_to_pay",	\args -> case args of
 		[x,y,z] -> have_to_pay z y x )
 
@@ -58,6 +65,14 @@ relations = [
 		[x,y] -> grow y x )
 	, ( "wanted_to_grow", \args -> case args of 
 		[x,y,z] -> wanted_to_grow z y x )
+	, ( "help", \args -> case args of 
+		[x,y] -> help y x )
+	, ( "wanted_to_help", \args -> case args of 
+		[x,y,z] -> wanted_to_help z y x )
+	, ( "make", \args -> case args of 
+		[x,y] -> make y x )
+	, ( "helped_to_make", \args -> case args of 
+		[x,y,z] -> helped_to_make z y x )
 	, ( "wanted_to_buy", \args -> case args of 
 		[x] -> (intransit . forgetful . unditrans) wanted_to_buy x
 		[x,y] -> (forgetful . unditrans) wanted_to_buy y x
