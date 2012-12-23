5bde8681aeae2abf10a742dc48a484783e571a94
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-12-23 12:37:50 +0000

    made,founded,set_up,predid1,2,3.

--- business/greatideas/internet/Story_Interpretation.hs
+++ business/greatideas/internet/Story_Interpretation.hs
@@ -3,12 +3,11 @@ module Story_Interpretation where
 import Model
 
 objects = [
-	( "oranges",	\[x] -> oranges x	)
-	, ( "citrus_fruit",	\[x] -> citrus_fruit x	)
-	 ,( "land",	\[x] -> land x	)
-	 , ( "experiment",	\[x] -> experiment x	)
-	 , ( "good_idea",	\[x] -> good_idea x	)
-	 , ( "a_good_price",	\[x] -> a_good_price x	)
+
+
+	 ( "experiment",	\[x] -> predid1 "experiment" x	)
+	 , ( "good_idea",	\[x] -> predid1 "good_idea" x	)
+	 , ( "a_good_price",	\[x] -> predid1 "a_good_price" x	)
 
 
 	]
@@ -23,6 +22,9 @@ inflections = [
  ( "want_to_grow", "wanted_to_grow" ),
  ( "helped", "help" ),
  ( "want_to_help", "wanted_to_help" ),
+ ( "made", "make" ),
+ ( "founded", "make" ),
+ ( "set_up", "make" ),
  ( "help_to_make", "helped_to_make" ),
  ( "want_to_buy", "wanted_to_buy" ),
  ( "want_to_look", "wanted_to_look" ),
@@ -32,47 +34,43 @@ inflections = [
  ]
 
 relations = [
-	( "successful",	\[x] -> successful x	)
-	, ( "unsuccessful",	\[x] -> unsuccessful x	)
-	, ( "finish",	\[x,y,z] -> finish z y x	)
+	( "successful",	\[x] -> predid1 "successful" x	)
+	, ( "unsuccessful",	\[x] -> predid1 "unsuccessful" x	)
+	, ( "finish",	\[x,y,z] -> predid3 "finish" z y x	)
 	, ( "pay",	\args -> case args of
 		[x,y,z,w] -> pay w z y x
 		[x,y,z] -> unditrans pay z y x
 		[x,y] -> (forgetful . unditrans) pay y x
 		[x] -> (intransit . forgetful . unditrans)  pay x )
 	, ( "have_to_pay",	\args -> case args of
-		[x,y,z] -> have_to_pay z y x )
+		[x,y,z] -> predid3 "have_to_pay" z y x )
 
 	, ( "offered_to_buy", \args -> case args of 
-		[x,y,z] -> offered_to_buy z y x
+		[x,y,z] -> predid3 "offered_to_buy" z y x
 		[x,y,z,w] -> offered_to_buy_from w z y x )
 
 	, ( "buy", \args -> case args of 
-		[x,y,z] -> buy z y x
-		[x,y] -> forgetful  buy y x
-		[x] -> (intransit . forgetful)  buy x )
+		[x,y,z] -> predid3 "buy" z y x
+		[x,y] -> forgetful (predid3 "buy") y x
+		[x] -> (intransit . forgetful) (predid3 "buy") x )
 
 	, ( "wanted_to_pay", \args -> case args of 
-		[x,y,z] -> wanted_to_pay z y x )
+		[x,y,z] -> predid3 "wanted_to_pay" z y x )
 
 	, ( "sell", \args -> case args of 
-		[x,y] -> forgetful sell y x
-		[x,y,z] -> sell z y x )
+		[x,y] -> forgetful (predid3 "sell") y x
+		[x,y,z] -> predid3 "sell" z y x )
 	, ( "wanted_to_sell", \args -> case args of 
 		[x,y,z] -> unditrans wanted_to_sell z y x
 		[x,y,z,w] -> wanted_to_sell w z y x )
-	, ( "grow", \args -> case args of 
-		[x,y] -> grow y x )
-	, ( "wanted_to_grow", \args -> case args of 
-		[x,y,z] -> wanted_to_grow z y x )
 	, ( "help", \args -> case args of 
-		[x,y] -> help y x )
+		[x,y] -> predid2 "help" y x )
 	, ( "wanted_to_help", \args -> case args of 
-		[x,y,z] -> wanted_to_help z y x )
+		[x,y,z] -> predid3 "wanted_to_help" z y x )
 	, ( "make", \args -> case args of 
-		[x,y] -> make y x )
+		[x,y] -> predid2 "make" y x )
 	, ( "helped_to_make", \args -> case args of 
-		[x,y,z] -> helped_to_make z y x )
+		[x,y,z] -> predid3 "helped_to_make" z y x )
 	, ( "wanted_to_buy", \args -> case args of 
 		[x] -> (intransit . forgetful . unditrans) wanted_to_buy x
 		[x,y] -> (forgetful . unditrans) wanted_to_buy y x
@@ -81,3 +79,4 @@ relations = [
 
 	]
 
+-- vim: set ts=8 sts=4 sw=4 noet:
