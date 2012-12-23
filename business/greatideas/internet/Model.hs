2f9612715903f307ec2af5f51db4d8cb53005fb3
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2012-12-23 12:35:35 +0000

    predid1,2,3. context, alex_tew, the_million_dollar_homepage, the one_million_people_page, facebook, mark_zuckerberg. female,male,role,child. onePlacers,twoPlacers,threePlacers.

--- business/greatideas/internet/Model.hs
+++ business/greatideas/internet/Model.hs
@@ -23,40 +23,43 @@ entities	=  [minBound..maxBound]
 characters :: [ (String, Entity) ]
 
 characters = [
-	( "pepsi",	P )
-	, ( "punjab",	AP )
-	, ( "punjabi_farmers",	AF )
-	, ( "the_punjabi_government",	AG )
+	( "alex_tew",	A )
+	, ( "the_million_dollar_homepage",	H )
+	, ( "the_one_million_people_page",	P )
+	, ( "facebook",	F )
+	, ( "mark_zuckerberg",	M )
 
 	]
 
-male, female :: OnePlacePred
-
-
-female	= pred1 []
-male	= pred1 [AF]
-role	= pred1 []
-child	= pred1 []
-
-oranges	= pred1 [O]
-citrus_fruit	= pred1 [O]
-land	= pred1 [L]
-
-money	= pred1 [M]
-good_idea	= pred1 [I]
-a_good_price	= pred1 [AP]
-
-good	= pred1 [AG]
-bad	= pred1 [AB]
-
-experiment	= pred1 [I]
-successful	= pred1 [P]
-unsuccessful	= pred1 [I]
-
+context :: [Entity]
+context = [A,H,P,F,M]
 
 namelist = map fst characters
-
 names = map swap characters
+
+predid1 :: String -> OnePlacePred
+predid2 :: String -> TwoPlacePred
+predid3 :: String -> ThreePlacePred
+predid1 name = maybe undefined id $ lookup name onePlacers
+predid2 name = maybe undefined id $ lookup name twoPlacers
+predid3 name = maybe undefined id $ lookup name threePlacers
+
+onePlacers = [
+	("male",	pred1 [A,M])
+	, ("female",	pred1 [])
+	, ("role",	pred1 [])
+
+	, ("money",	pred1 [M])
+	, ("good_idea",	pred1 [I])
+	, ("a_good_price",	pred1 [AP])
+
+	, ("good",	pred1 [AG])
+	, ("bad",	pred1 [AB])
+
+	, ("experiment",	pred1 [I])
+	, ("successful",	pred1 [P])
+	, ("unsuccessful",	pred1 [I])
+	]
 
 type OnePlacePred	= Entity -> Bool
 type TwoPlacePred	= Entity -> Entity -> Bool
@@ -69,15 +72,15 @@ list2OnePlacePred xs	= \ x -> elem x xs
 pred1 :: [Entity] -> OnePlacePred
 pred1	= flip elem
 
-person, thing :: OnePlacePred
 
-person	= \ x -> (male x || female x || role x || x == Unspec)
+person, thing :: OnePlacePred
+person	= \ x -> (predid1 "male" x || predid1 "female" x || predid1 "role" x || x == Unspec)
 thing	= \ x -> (x == Unspec || not ( person x ) )
 
-boy	= \x -> male x && child x
-isMan	= \x -> ( not $ boy x ) && male x
-isGirl	= \x -> ( female x && child x )
-isWoman	= \x -> ( not $ isGirl x ) && female x
+-- boy	= \x -> male x && child x
+-- isMan	= \x -> ( not $ boy x ) && male x
+-- isGirl	= \x -> ( female x && child x )
+-- isWoman	= \x -> ( not $ isGirl x ) && female x
 boss = pred1 $ map fst management
 
 pred2 :: [(Entity,Entity)] -> TwoPlacePred
@@ -87,54 +90,52 @@ pred3 xs	= curry3 ( `elem` xs )
 pred4 xs	= curry4 ( `elem` xs )
 pred5 xs	= curry5 ( `elem` xs )
 
-acquainted	= [(P,O),(P,AG),(AF,AG),(P,AF)]
-know	= pred2 $ acquainted ++ map swap acquainted
-have	= pred2 $ possessions ++ management ++ family ++ patronage
-		-- ++ ( map (\x->(x,N) ) $ filter person entities ) -- personality
+twoPlacers = [
+	("know",	pred2 $ acquainted ++ map swap acquainted)
+	, ("have",	pred2 $ possessions ++ management ++ family ++ patronage)
+	, ("help",	pred2 [(AG,AF)])
+	, ("make",	pred2 [(A,H),(M,F)])
+	, ("said",	pred2 $ map (\x->(agent x, theme x) ) comms)
+	, ("asked",	pred2 $ map (\x->(agent x, recipient x) ) comms)
+	, ("talk_with_or_about",	pred2 $ map (\x->(agent x, recipient x) ) comms
+              ++  map (\(agent,theme,recipient)->(agent, theme) ) comms)
+	, ("recite",	pred2 $ map ( \x -> (agent x, theme x) ) comms)
+	, ("work_where",	pred2 $ map (\x -> (patient x, agent x) ) work)
+	, ("work_as",	pred2 $ map (\x -> (patient x, location x) ) work)
+	]
+
+threePlacers = [
+	("finish",	pred3 [])
+	, ("buy",	pred3 $ map (\x -> (agent4 x, theme4 x, provider4 x) ) purchases)
+	, ("sell",	pred3 [(AF,O,P)])
+	, ("wanted_to_help",	pred3 [(AG,AG,AF)])
+	, ("helped_to_make",	pred3 [(AG,AF,M)])
+	, ("wanted_to_pay",	pred3 [])
+	, ("have_to_pay",	pred3 [])
+	, ("offered_to_buy",	pred3 [(P,P,O)])
+	, ("gave",	pred3 giving)
+	, ("got",	pred3 $ map (\x -> (recipient x, patient x, agent x) ) giving)
+	, ("ask_about",	pred3 $ map (\x->(agent x, recipient x, theme x) ) comms)
+	, ("talk_with_about",	pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
+				++ comms)
+	, ("told",	pred3 comms)
+	]
+
 family	= []
-patronage = []
-finish	= pred3 []
+patronage	= []
+acquainted	= [(P,O),(P,AG),(AF,AG),(P,AF)]
+
 purchases	= [(P,O,AF,Unspec)]
-buy	= pred3 $ map (\x -> (agent4 x, theme4 x, provider4 x) ) purchases
 pay = pred4 $ map (\x -> (agent4 x, provider4 x, theme4 x, purpose4 x) ) purchases
 
-wanted = buy
+wanted = predid3 "buy"
 wanted_to_buy :: FourPlacePred
 wanted_to_buy	= pred4 $
 	map ( \x -> (agent4 x, agent4 x, theme4 x, provider4 x) ) purchases
 
-sell :: ThreePlacePred
-sell	= pred3 [(AF,O,P)]
-
 wanted_to_sell :: FourPlacePred
 wanted_to_sell	= pred4 [(AF,AF,O,P)]
 
-grow :: TwoPlacePred
-grow	= pred2 [(AF,O),(AG,O)]
-
-wanted_to_grow :: ThreePlacePred
-wanted_to_grow	= pred3 [(P,AF,O),(P,AG,O),(AF,AF,O),(AG,AG,O),(AG,AF,O)]
-
-help :: TwoPlacePred
-help	= pred2 [(AG,AF)]
-
-wanted_to_help :: ThreePlacePred
-wanted_to_help	= pred3 [(AG,AG,AF)]
-
-make :: TwoPlacePred
-make	= pred2 [(AF,M)]
-
-helped_to_make :: ThreePlacePred
-helped_to_make	= pred3 [(AG,AF,M)]
-
-wanted_to_pay :: ThreePlacePred
-wanted_to_pay	= pred3 []
-
-have_to_pay :: ThreePlacePred
-have_to_pay = pred3 []
-
-offered_to_buy :: ThreePlacePred
-offered_to_buy	= pred3 [(P,P,O)]
 offered_to_buy_from :: FourPlacePred
 offered_to_buy_from	= pred4 [(P,P,O,AF)]
 
@@ -158,22 +159,9 @@ instrument = recipient
 origin	= theme
 destination = recipient
 
-gave	= pred3 giving
-got	= pred3 $ map (\x -> (recipient x, patient x, agent x) ) giving
 
-said	= pred2 $ map (\x->(agent x, theme x) ) comms
-asked	= pred2 $ map (\x->(agent x, recipient x) ) comms
-ask_about = pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
-talk_with_or_about	= pred2 $ map (\x->(agent x, recipient x) ) comms
-              ++  map (\(agent,theme,recipient)->(agent, theme) ) comms
-talk_with_about = pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
-		++ comms
-told	= pred3 comms
-recite = pred2 $ map ( \x -> (agent x, theme x) ) comms
 
 worker	= pred1 $ map patient work
-work_where	= pred2 $ map (\x -> (patient x, agent x) ) work
-work_as = pred2 $ map (\x -> (patient x, location x) ) work
 
 giving	= [(AF,L,AG)]
 comms	= [ (P,O,AG),(AG,O,AF),(AG,L,AF) ]
@@ -208,3 +196,5 @@ passivize4 r = \x y z -> or ( map (\u -> r u x y z ) entities )
 
 self ::  (a -> a -> b) -> a -> b
 self p	= \ x -> p x x 
+
+-- vim: set ts=8 sts=4 sw=4 noet:
