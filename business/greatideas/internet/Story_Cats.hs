c190913a40fe44268e7febe6dd5af0a0ca82ff75
drbean <drbean@users.noreply.github.com>
2013-01-05 12:39:02 +0000

    advertisers,radio_and_television,website,business_management,money,studied,promoted,bought,buy,sold,sell.

--- business/greatideas/internet/Story_Cats.hs
+++ business/greatideas/internet/Story_Cats.hs
@@ -16,11 +16,20 @@ nouns = [
 	 [Cat "experiment"	"CN" [Thrd,Neutr,Sg] []]
 	 , [Cat "good_idea"	"CN" [Thrd,Neutr,Sg] []]
 	 , [Cat "a_good_price"	"NP" [Thrd,Neutr,Sg] []]
-	 , [Cat "advertisers"	"NP" [Thrd,MascOrFem,Pl] []]
+	 , [Cat "advertisers"	"CN" [Thrd,MascOrFem,Pl] []]
 	 , [Cat "advertising_space"	"NP" [Thrd,Neutr,Sg] []]
+	 , [Cat "media"	"CN" [Thrd,Neutr,Sg] []]
+	 , [Cat "radio_and_television"	"NP" [Thrd,Neutr,Sg] []]
+	 , [Cat "website"	"CN" [Thrd,Neutr,Sg] []]
+	 , [Cat "business_management"	"NP" [Thrd,Neutr,Sg] []]
+	, [Cat "money"   "NP" [Sg,Neutr,Thrd]   []]
 	]
 
 verbs = [
+	[Cat "studied"	"VP" [Tense] [],
+		Cat "studied"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "study"	"VP" [Infl] [],
+		Cat "study"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "helped"	"VP" [Tense] [],
 		Cat "helped"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "help"	"VP" [Infl] [],
@@ -29,22 +38,46 @@ verbs = [
 	[Cat "found"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "set_up"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "set_up"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "promoted"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "promoted"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []]],
+	[Cat "promote"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
+		Cat "promote"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []]],
 	[Cat "bought"	"VP" [Tense] [],
 		Cat "bought"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
 		Cat "bought"	"VP" [Tense] [Cat "_" "PP" [From] []],
 		Cat "bought"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [From] []],
+		Cat "bought"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] [],
 						Cat "_" "PP" [From] []]],
 	[Cat "buy"	"VP" [Infl] [],
 		Cat "buy"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
 		Cat "buy"	"VP" [Infl] [ Cat "_" "PP" [From] []],
 		Cat "buy"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [From] []],
+		Cat "buy"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] [],
 						Cat "_" "PP" [From] []]],
 	[Cat "sold"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "sold"	"VP" [Tense] [Cat "_" "PP" [To] []],
 		Cat "sold"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
-						Cat "_" "PP" [To] []]],
+						Cat "_" "PP" [To] []],
+		Cat "sold"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []],
+		Cat "sold"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] [],
+						Cat "_" "PP" [On] []]],
 	[Cat "sell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []],
+		Cat "sell"	"VP" [Infl] [Cat "_" "PP" [To] []],
 		Cat "sell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
-						Cat "_" "PP" [To] []]],
+						Cat "_" "PP" [To] []],
+		Cat "sell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []],
+		Cat "sell"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] [],
+						Cat "_" "PP" [On] []]],
 	[Cat "paid"	"VP" [Tense] [],
 		Cat "paid"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []],
 		Cat "paid" "VP" [Tense]  [Cat "_" "NP" [AccOrDat] [],
