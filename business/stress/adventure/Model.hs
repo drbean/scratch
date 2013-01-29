40ec5218d0fd53468e65fb549992435a509b2463
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-01-29 03:00:06 +0000

    cherry-picked  stress/Model.hs stress/adventure/Model.hs

--- business/stress/adventure/Model.hs
+++ business/stress/adventure/Model.hs
@@ -8,26 +8,35 @@ data Entity	= A | B | C | D | E | F | G
             | H | I | J | K | L | M | N 
             | O | P | Q | R | S | T | U 
             | V | W | X | Y | Z 
-            | AA | BB | CC | DD | EE | FF | GG 
-            | HH | II | JJ | KK | LL | MM | NN 
-            | OO | PP | QQ | RR | SS | TT | UU 
-            | VV | WW | XX | YY | ZZ | Someone | Something | Some12 | Unspec
+            | Someone | Something | Unspec
      deriving (Eq,Show,Bounded,Enum,Ord)
 
 entities :: [Entity]
 entities	=  [minBound..maxBound] 
 
 
+--alice, rex, kelley, judy, rock
+--                                                :: Entity
+
 characters :: [ (String, Entity) ]
 
 characters = [
-	( "ellen",	E ),
+	( "ellen_macarthur",	E ),
 	( "dr_bean",	T ),
 	( "steve",	S ),
+	( "steve_fossett",	S ),
 	( "cusp",	K )
 
 	]
 
+classes :: [String]
+classes = [ "manager", "advertisers", "advertising_space", "business_management", "radio_and_television" ]
+
+lack_of_control	= [C]
+uncertainty	= [U]
+lack_of_support	= [H]
+pressure	= pred1 [P]
+stress	= pred1 [R]
 
 adventurer	= pred1 [S,E]
 teacher	= pred1 [T]
@@ -53,23 +62,57 @@ pressure	= pred1 [PP]
 framework	= pred1 [K]
 useful	= framework
 
+namelist :: [String]
 namelist = map fst characters
+names :: [(Entity, String)]
+names = map swap characters
 
 
-male, female :: OnePlacePred
+predid1 :: String -> OnePlacePred
+predid2 :: String -> TwoPlacePred
+predid3 :: String -> ThreePlacePred
+predid4 :: String -> FourPlacePred
+predid1 name = lookupPred name onePlacers where
+	lookupPred name []	= error $ "no \"" ++ name ++ "\" one-place predicate."
+	lookupPred name ((n,p):_) | n == name	= p
+	lookupPred name (i:is) = lookupPred name is
+predid2 name = lookupPred name twoPlacers where
+	lookupPred n []	= error $ "no \"" ++ name ++ "\" two-place predicate."
+	lookupPred n ((name,pred):is) | n == name	= pred
+	lookupPred n (i:is) = lookupPred name is
+predid3 name = lookupPred name threePlacers where
+	lookupPred n []	= error $ "no \"" ++ name ++ "\" three-place predicate."
+	lookupPred n ((name,pred):is) | n == name	= pred
+	lookupPred n (i:is) = lookupPred name is
+predid4 name = lookupPred name fourPlacers where
+	lookupPred n []	= error $ "no \"" ++ name ++ "\" four-place predicate."
+	lookupPred n ((name,pred):is) | n == name	= pred
+	lookupPred n (i:is) = lookupPred name is
 
-male	= pred1 [T,S]
-female	= pred1 [E]
+onePlacers :: [(String, OnePlacePred)]
+onePlacers = [
+	("true",	pred1 entities)
+	, ("false",	pred1 [])
+	, ("male",	pred1 [S,Q,V,C,O,L,I])
+	, ("female",	pred1 [E,P,N])
+	, ("manager",	pred1 [Q,O])
+	, ("boss",	pred1 $ map fst management)
+	, ("role",	pred1 [])
+	, ("person",	person)
+	, ("thing",	thing)
 
-boy x	= False
-isMan   = \x -> ( not $ boy x ) && male x
-isGirl  = \x -> False
-isWoman = \x -> ( not $ isGirl x ) && female x
+	, ("good",	pred1 [])
+	, ("bad",	pred1 [C,U,H,P,R])
 
+	, ("experiment",	pred1 [H,P,F])
+	, ("successful",	pred1 [H,F])
+	, ("unsuccessful",	pred1 [P])
+	]
 
 type OnePlacePred	= Entity -> Bool
 type TwoPlacePred	= Entity -> Entity -> Bool
 type ThreePlacePred	= Entity -> Entity -> Entity -> Bool
+type FourPlacePred	= Entity -> Entity -> Entity -> Entity -> Bool
 
 list2OnePlacePred :: [Entity] -> OnePlacePred
 list2OnePlacePred xs	= \ x -> elem x xs
@@ -77,10 +120,9 @@ list2OnePlacePred xs	= \ x -> elem x xs
 pred1 :: [Entity] -> OnePlacePred
 pred1	= flip elem
 
-people, things :: OnePlacePred
-
-people	= \ x -> (male x || female x || x == Unspec)
-things	= \ x -> (x == Unspec || not ( people x ) )
+person, thing :: OnePlacePred
+person	= \ x -> (predid1 "male" x || predid1 "female" x || predid1 "role" x || x == Someone)
+thing	= \ x -> (x == Unspec || x == Something || not ( person x ) )
 
 pred2 :: [(Entity,Entity)] -> TwoPlacePred
 pred3 :: [(Entity,Entity,Entity)] -> ThreePlacePred
@@ -88,23 +130,68 @@ pred2 xs	= curry ( `elem` xs )
 pred3 xs	= curry3 ( `elem` xs )
 pred4 xs	= curry4 ( `elem` xs )
 
+
+twoPlacers = [
+	("know",	pred2 $ acquainted ++ map swap acquainted)
+	, ("have",	pred2 $ possessions ++ management ++ features)
+	, ("help",	pred2 [])
+	, ("said",	pred2 $ map (\x->(agent x, theme x) ) comms)
+	, ("asked",	pred2 $ map (\x->(agent x, recipient x) ) comms)
+	, ("talk_with_or_about",	pred2 $ map (\x->(agent x, recipient x) ) comms
+              ++  map (\(agent,theme,recipient)->(agent, theme) ) comms)
+	, ("recite",	pred2 $ map ( \x -> (agent x, theme x) ) comms)
+	--, ("work_where",	pred2 $ map (\x -> (patient x, agent x) ) work)
+	--, ("work_as",	pred2 $ map (\x -> (patient x, location x) ) work)
+	, ("studied",	\a t -> case (a,t) of
+		(A,G) -> False; otherwise -> pred2 studies a t)
+	]
+
 -- stressful :: [(Entity, Entity)] -> Bool
 -- stressful = \ x -> ( x `elem` [control, uncertainty, support, pressure] &&
 -- 			( not $ null x ) )
+threePlacers = [
+	("finish",	pred3 [])
+	, ("make",	pred3 $
+		foldl (\ms (a,t,r,_) -> (a,t,r):(a,r,t):ms ) [] makings)
+	, ("gave",	pred3 giving)
+	, ("got",	pred3 $ map (\x -> (recipient x, patient x, agent x) ) giving)
+	, ("ask_about",	pred3 $ map (\x->(agent x, recipient x, theme x) ) comms)
+	, ("talk_with_about",	pred3 $ map (\x->(agent x, recipient x, theme x) ) comms
+				++ comms)
+	, ("told",	pred3 comms)
+	, ("wanted_to_study", pred3 $ map (\(a,t) -> (a,a,t)) studies)
+	]
 
 -- control	= [(Unspec,Unspec)]
 -- uncertainty	= [(Unspec,Unspec)]
 -- support	= [(Unspec,E)]
 --(pressurizer,pressured)
-pressurepoints	= []
-stress_causes	= [(S,UU),(S,PP),(S,CC),(E,UU),(E,PP)]
+hotspots	= []
+
+fourPlacers = [
+	("get",	pred4 $ map (\x -> (agent4 x, theme4 x, provider4 x, location4 x)
+				) services ++
+			map (\x -> (agent4 x, provider4 x, theme4 x, location4 x)
+				) services )
+	, ("give", pred4 $ foldl (\ss (a,t,p,l) -> (l,t,a,l): (p,t,a,l): (p,t,l,a):
+					ss ) [] services)
+	, ("wanted_to_make",	pred4 $ map (\(a,t,_,p) -> (a,a,p,t)) makings)
+	]
 
 -- circumnavigator, vehicle, accomplishment
 circumnavigations	= [(S,B),(S,P),(S,Y),(E,Y)]
 possessions	= []
 recruitment	= []
 appreciation	= []
-supervision	= []
+family	= []
+patronage	= []
+acquainted	= []
+studies	= []
+-- (agent,theme,result,aim)
+makings	= []
+features	= []
+services	= []
+pay = pred4 $ map (\x -> (agent4 x, provider4 x, theme4 x, purpose4 x) ) services
 
 supervisor	= pred1 $ map fst supervision
 boss	= supervisor
@@ -185,23 +272,46 @@ offend	= pred2 $ ( map (\x -> (agent x, recipient x) ) offenses ) ++
 		( map (\x -> (theme x, recipient x) ) offenses )
 anger = offend
 
-pressurize = pred2 pressurepoints
+pressurize = pred2 hotspots
 gave	= pred3 giving
 got	= pred3 $ map (\x -> (recipient x, patient x, agent x) ) giving
-sold	= pred2 $ map (\x -> (agent x, theme x) ) selling
+wanted	= got
 
 told	= pred3 comms
 
 recite = pred2 $ map ( \x -> (agent x, theme x) ) comms
+giving	= map (\(a,t,p,_) -> (a,t,p) ) services
 
 agent4, theme4, recipient4, location4 :: (Entity,Entity,Entity,Entity) -> Entity
-agent4 (a,_,_,_) = a
-location4 (_,l,_,_) = l
-theme4 (_,_,t,_) = t
-recipient4 (_,_,_,r) = r
+agent4 (a,_,_,_)	= a
+theme4 (_,t,_,_)	= t
+recipient4 (_,_,r,_)	= r
+provider4	= recipient4
+location4 (_,_,_,l)	= l
+purpose4	= location4
+aim4	= purpose4
+result4	= recipient4
 
-forgetful :: ThreePlacePred -> TwoPlacePred
-forgetful r u v = or ( map ( r u v ) entities )
+-- (teacher,school(location),subject,student)
+schooling = [(Unspec,Unspec,V,D)]
+studied = pred3 $ map ( \x -> (recipient4 x, theme4 x, location4 x) )
+				schooling
+studied_what = pred2 $ map (\x -> (recipient4 x, theme4 x) ) schooling
+studied_where = pred2 $ map (\x -> (recipient4 x, location4 x) ) schooling
+teach = pred3 $ map (\x -> (agent4 x, theme4 x, recipient4 x) ) schooling
+teach_what = forgetful teach
+teach_who = pred2 $ map (\x -> (agent4 x, recipient4 x) ) schooling
+student = pred1 $ map recipient4 schooling
+
+forgetful4 :: FourPlacePred -> ThreePlacePred
+forgetful4 r u v w = or ( map ( r u v w ) entities )
+
+forgetful3 :: ThreePlacePred -> TwoPlacePred
+forgetful3 r u v = or ( map ( r u v ) entities )
+
+forgetful2 :: TwoPlacePred -> OnePlacePred
+forgetful2 r u = or ( map ( r u ) entities )
+
 passivize :: TwoPlacePred -> OnePlacePred
 passivize r	= \ x -> or ( map ( flip  r x ) entities )
 
