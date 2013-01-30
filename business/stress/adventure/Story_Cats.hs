2758097cda3d25deccbf361c9722e0816479f3ba
drbean <drbean@users.noreply.github.com>
2013-01-30 03:45:09 +0000

    nouns,names,verbs,,aux,adjs,advs

--- business/stress/adventure/Story_Cats.hs
+++ business/stress/adventure/Story_Cats.hs
@@ -10,16 +10,16 @@ multipart_names = [
 	(["Dr","Bean"],	"Dr_Bean")
 	]
 
-proper_names, object_names, story_verbs :: Lexset
+names, nouns, verbs :: Lexset
 
-proper_names = [
+names = [
 	[Cat "cusp" "NP" [Thrd,Neutr,Sg] []],
 	[Cat "ellen" "NP" [Thrd,Fem,Sg] []],
 	[Cat "dr_bean" "NP" [Thrd,Masc,Sg] []],
 	[Cat "steve" "NP" [Thrd,Masc,Sg] []]
 	]
 
-object_names = [
+nouns = [
 
 	[Cat "control"	"NP" [Thrd,Neutr,Sg] []],
 	[Cat "lack_of_control"	"NP" [Thrd,Neutr,Sg] []],
@@ -38,7 +38,7 @@ object_names = [
 	[Cat "world"	"CN" [Thrd,Neutr,Sg] []]
 	]
 
-story_verbs = [
+verbs = [
 	[Cat "helped"	"VP" [Tense] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "help"	"VP" [Infl] [Cat "_" "NP" [AccOrDat] []]],
 	[Cat "felt_stress"	"VP" [Tense] [Cat "_" "PP" [BecauseOf] []],
@@ -61,11 +61,13 @@ story_verbs = [
 						Cat "_" "PP" [In] []]]
 	]
 
-story_aux = [
+aux = [
 	]
 
-story_adjs = [
+adjs = [
 	[Cat "stressful"	"ADJ" [] []]
 	]
 
-
+advs = [
+	-- [Cat "slowly"	"ADV" [] []]
+	]
