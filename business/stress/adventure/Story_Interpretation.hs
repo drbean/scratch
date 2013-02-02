17d602ef91539295a0afd0d00b13a60202fa0902
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-02-02 14:03:13 +0000

    commas,fly,flew,sail,sailed,powered_aircraft,plane.relations:sailed,flew,forgetful,predid5

--- business/stress/adventure/Story_Interpretation.hs
+++ business/stress/adventure/Story_Interpretation.hs
@@ -3,38 +3,51 @@ module Story_Interpretation where
 import Model
 
 objects = [
-	( "adventurer",	\[x]	-> predid1 "adventurer" ),
-	( "teacher",	\[x]	-> predid1 "teacher" ),
+	( "adventurer",	\[x]	-> predid1 "adventurer" x )
+	, ( "teacher",	\[x]	-> predid1 "teacher" x )
 
-	( "lack_of_control",	\[x] -> predid1 "lack_of_control" ),
-	( "uncertainty",	\[x] -> predid1 "uncertainty" ),
-	( "lack_of_support",	\[x] -> predid1 "lack_of_support" ),
-	( "pressure",	\[x] -> predid1 "pressure" ),
+	, ( "lack_of_control",	\[x] -> predid1 "lack_of_control" x )
+	, ( "uncertainty",	\[x] -> predid1 "uncertainty" x )
+	, ( "lack_of_support",	\[x] -> predid1 "lack_of_support" x )
+	, ( "pressure",	\[x] -> predid1 "pressure" x )
+	, ( "stress",	\[x] -> predid1 "stress" x )
+	, ( "stressful",	\[x] -> predid1 "stressful" x )
 
-	( "world",	\[x] -> predid1 "world" ),
-	( "plane",	\[x] -> predid1 "plane" ),
-	( "boat",	\[x] -> predid1 "boat" ),
-	( "glider",	\[x] -> predid1 "glider" ),
-	( "balloon",	\[x] -> predid1 "balloon" )
+	, ( "world",	\[x] -> predid1 "world" x )
+	, ( "plane",	\[x] -> predid1 "plane" x )
+	, ( "boat",	\[x] -> predid1 "boat" x )
+	, ( "glider",	\[x] -> predid1 "glider" x )
+	, ( "balloon",	\[x] -> predid1 "balloon" x )
+	, ( "aircraft",	\[x] -> predid1 "aircraft" x )
 
 	]
 
 inflections = [
- ( "adventurers", "adventurer" ),
- ( "feel_stress", "felt_stress" ),
- ( "sailed", "sail" ),
- ( "flew", "fly" )
+ ( "adventurers", "adventurer" )
+ , ( "feel_stress", "felt_stress" )
+ , ( "sail", "sailed" )
+ , ( "fly", "flew" )
+ , ( "flew", "fly" )
+ , ( "powered_aircraft", "plane" )
 
  ]
 
 relations = [
-	( "useful",	\[x]	-> predid1 "useful" )
+	( "useful",	\[x]	-> predid1 "useful" x )
 
 	-- ( "felt_stress",	\args -> case args of
 		-- [x,y] -> predid2 "cause_stress" y x ),
 		-- [x] -> predid1 "feel_stress" x ),
 	-- ( "put_pressure",	\[x,y]	-> pressurize y x	),
-	-- ( "sail",	\args -> case args of [x,y] -> sail_around y x; [x,y,z] -> sail_around_in z y x	),
-	-- ( "fly",	\args -> case args of [x,y] -> fly_around y x; [x,y,z] -> fly_around_in z y x	),
+	, ( "sailed",	\args -> case args of
+		[x,y,z,w,v] -> predid5 "sailed" v w z y x
+		[x,y,z,w] -> (forgetful5 . predid5 ) "sailed" w z y x
+		[x,y,z] -> (forgetful4 . forgetful5 . predid5 ) "sailed" z y x
+		[x,y] -> (forgetful3 . forgetful4 . forgetful5 . predid5 ) "sailed" y x )
+	, ( "flew",	\args -> case args of
+		[x,y,z,w,v] -> predid5 "flew" v w z y x
+		[x,y,z,w] -> (forgetful5 . predid5 ) "flew" w z y x
+		[x,y,z] -> (forgetful4 . forgetful5 . predid5 ) "flew" z y x
+		[x,y] -> (forgetful3 . forgetful4 . forgetful5 . predid5 ) "flew" y x )
 	]
 
