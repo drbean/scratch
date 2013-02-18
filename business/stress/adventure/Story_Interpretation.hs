353589551689a8ae483ac314b75a73cdf3ab1adf
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-02-18 08:14:32 +0000

    sailed x y

--- business/stress/adventure/Story_Interpretation.hs
+++ business/stress/adventure/Story_Interpretation.hs
@@ -44,7 +44,7 @@ relations = [
 		[x,y,z,w,v] -> predid5 "sailed" v w z y x
 		[x,y,z,w] -> (forgetful5 . predid5 ) "sailed" w z y x
 		[x,y,z] -> (forgetful4 . forgetful5 . predid5 ) "sailed" z y x
-		[x,y] -> (forgetful3 . forgetful4 . forgetful5 . predid5 ) "sailed" y x )
+		[x,y] -> (forgetful3 . forgetful4 . forgetful5 . predid5 ) "sailed" x y )
 	, ( "flew",	\args -> case args of
 		[x,y,z,w,v] -> predid5 "flew" v w z y x
 		[x,y,z,w] -> (forgetful5 . predid5 ) "flew" w z y x
