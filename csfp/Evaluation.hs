defafaf158f7665bf206384e46572626e0de1214
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-02-18 09:28:17 +0000

    have =  Leaf (Cat "have" "V" [] [])

--- csfp/Evaluation.hs
+++ csfp/Evaluation.hs
@@ -459,7 +459,7 @@ intCN (Branch (Cat _    "RCN" _ _) [cn,rel]) = case rel of
     	-> case (phon (t2c prep)) of
 		"with" -> \owner ->  (intNP np (\object-> conj (intCN cn owner) (blowupTV have "had" owner object))) 
 		"in" -> intCN cn
-		where have = Branch (Cat "_" "VP" [] []) [Leaf (Cat "have" "V" [] []),np]
+		where have =  Leaf (Cat "have" "V" [] [])
 --intCN (Branch (Cat _    "CN" _ _) [cn,ofpos,np]) =
 --    \x y -> exists `conj` intCN x `conj` intNP np (\thing -> predid2 "had" [x, thing])
 
