787eb7176f65d00e8e9f7c8d98c63d2f695d1e12
drbean <drbean@users.noreply.github.com>
2013-04-01 00:51:10 +0000

    TMPL:locationTMPL_cgi.pl

--- pages/htmlexerciselist.pl
+++ pages/htmlexerciselist.pl
@@ -64,10 +64,10 @@ my $storystring = '
 </p>
 <p>
 Fill in the blanks in the <a 
-href=http://203.64.184.141/cgi-bin/<TMPL> $location </TMPL>/script_files/dic_cgi.pl/login?exercise=<TMPL> $id </TMPL>><TMPL> $id </TMPL></a> exercise. (You will be asked to log in.)
+href=http://203.64.184.141/cgi-bin/<TMPL> $location </TMPL>/script/<TMPL> $location </TMPL>_cgi.pl/login?exercise=<TMPL> $id </TMPL>><TMPL> $id </TMPL></a> exercise. (You will be asked to log in.)
 </p>
 <p>
-If that link doesn\'t work, try the <a href="http://sac.nuu.edu.tw/cgi-bin/<TMPL> $location </TMPL>/script_files/dic_cgi.pl/login?exercise=<TMPL> $id </TMPL>"> alternative <TMPL> $id </TMPL> exercise link </a>.
+If that link doesn\'t work, try the <a href="http://sac.nuu.edu.tw/cgi-bin/<TMPL> $location </TMPL>/script/<TMPL> $location </TMPL>_cgi.pl/login?exercise=<TMPL> $id </TMPL>"> alternative <TMPL> $id </TMPL> exercise link </a>.
 </div>
 
 ';
