ca124774ed7b209a251167ef5151c2d3803bb71e
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-04-07 06:52:27 +0000

    OnModCommand, Ping oftc twice at intervals of 10 secs

--- znc/pingIrc.pm
+++ znc/pingIrc.pm
@@ -1,7 +1,7 @@
 package pingIrc;
 
 # Created: 04/07/2013 01:33:27 PM
-# Last Edit: 2013 Apr 07, 02:30:57 PM
+# Last Edit: 2013 Apr 07, 02:51:45 PM
 # $Id$
 
 =head1 NAME
@@ -30,9 +30,9 @@ use base 'ZNC::Module';
 sub module_types { $ZNC::CModInfo::UserModule }
 
 my $server = "chat.oftc.net";
-sub OnBoot {
-    my ($self) = @_;
-    $self->CreateTimer( \&ping, interval => 60, cycles => 0, context => $server );
+sub OnModCommand {
+    my ($self, $cmd) = @_;
+    $self->CreateTimer( \&ping, interval => 10, cycles => 2, context => $server );
 }
 
 sub ping {
