ff14602a4fb910d83934a5905c9e7e0eaa164b33
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2013-04-12 11:17:38 +0000

    Did Dr Bean buy shoes? 4Ps

---
+++ business/marketing/shoes/Story_Cats.hs
@@ -0,0 +1,97 @@
+module Story_Cats where
+
+import Parsing
+
+names, nouns, verbs, aux, adjs, advs :: Lexset
+
+names = [
+	[Cat "dr_bean"	"NP" [Thrd,Masc,Sg] []]
+	, [Cat "quanlian"	"NP" [Thrd,Neutr,Sg] []]
+	, [Cat "quanjiafu"	"NP" [Thrd,Neutr,Sg] []]
+	]
+
+nouns = [
+	[Cat "shoes"	"CN" [Thrd,Neutr,Pl] []]
+	, [Cat "oil"	"NP" [Thrd,Neutr,Sg] [],
+		Cat "oil"	"CN" [Thrd,Neutr,Sg] []]
+	, [Cat "milk"	"NP" [Thrd,Neutr,Sg] [],
+		Cat "milk"	"CN" [Thrd,Neutr,Sg] []]
+	, [Cat "bananas"	"CN" [Thrd,Neutr,Pl] []]
+	, [Cat "eggs"	"CN" [Thrd,Neutr,Pl] []]
+	, [Cat "rice"	"NP" [Thrd,Neutr,Sg] [],
+		Cat "rice"	"CN" [Thrd,Neutr,Sg] []]
+	, [Cat "fruit_store"	"CN" [Thrd,Neutr,Sg] []]
+
+	, [Cat "a_good_price"	"NP" [Thrd,Neutr,Sg] []]
+	, [Cat "money"   "NP" [Sg,Neutr,Thrd]   []]
+	, [Cat "price"	"NP" [Thrd,Neutr,Sg] []]
+
+	]
+
+verbs = [
+	[Cat "liked"	"V" [Tense] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "like"	"V" [Infl] [Cat "_" "NP" [AccOrDat] []]],
+	[Cat "bought"	"V" [Tense] [],
+		Cat "bought"	"V" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "bought"	"V" [Tense] [Cat "_" "PP" [From] []],
+		Cat "bought"	"V" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [From] []],
+		Cat "bought"	"V" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [At] []]],
+	[Cat "buy"	"V" [Infl] [],
+		Cat "buy"	"V" [Infl] [Cat "_" "NP" [AccOrDat] []],
+		Cat "buy"	"V" [Infl] [ Cat "_" "PP" [From] []],
+		Cat "buy"	"V" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [From] []],
+		Cat "buy"	"V" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [At] []]],
+	[Cat "sold"	"V" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "sold"	"V" [Tense] [Cat "_" "PP" [To] []],
+		Cat "sold"	"V" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] []],
+		Cat "sold"	"V" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []],
+		Cat "sold"	"V" [Tense] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] [],
+						Cat "_" "PP" [On] []]],
+	[Cat "sell"	"V" [Infl] [Cat "_" "NP" [AccOrDat] []],
+		Cat "sell"	"V" [Infl] [Cat "_" "PP" [To] []],
+		Cat "sell"	"V" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] []],
+		Cat "sell"	"V" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [On] []],
+		Cat "sell"	"V" [Infl] [Cat "_" "NP" [AccOrDat] [],
+						Cat "_" "PP" [To] [],
+						Cat "_" "PP" [On] []]],
+	[Cat "paid"	"V" [Tense] [],
+		Cat "paid"	"V" [Tense] [Cat "_" "NP" [AccOrDat] []],
+		Cat "paid" "V" [Tense]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "NP" [AccOrDat] []],
+		Cat "paid" "V" [Tense]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "PP" [For] []],
+		Cat "paid" "V" [Tense]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "PP" [For] []]],
+	[Cat "pay"	"V" [Infl] [],
+		Cat "pay"	"V" [Infl] [Cat "_" "NP" [AccOrDat] []],
+		Cat "pay" "V" [Infl]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "NP" [AccOrDat] []],
+		Cat "pay" "V" [Infl]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "PP" [For] []],
+		Cat "pay" "V" [Infl]  [Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "NP" [AccOrDat] [],
+                                                Cat "_" "PP" [For] []]]
+
+	]
+
+aux = [
+	]
+
+adjs = [
+	[Cat "successful"	"ADJ" [] []]
+	, [Cat "unsuccessful"	"ADJ" [] []]
+	]
+
+advs = [
+	-- [Cat "slowly"	"ADV" [] []]
+	]
---
+++ business/marketing/shoes/Story_Interpretation.hs
@@ -0,0 +1,89 @@
+module Story_Interpretation where 
+
+import Model
+
+objects :: [(String, [Entity] -> Bool)]
+objects = [
+	 ( "good_idea",	\[x] -> predid1 "good_idea" x	)
+	 , ( "a_good_price",	\[x] -> predid1 "a_good_price" x	)
+	, ( "teacher",	\[x]	-> predid1 "teacher" x )
+	, ( "shoes",	\[x]	-> predid1 "shoes" x	)
+	, ( "oil",	\[x]	-> predid1 "oil" x	)
+	, ( "milk",	\[x]	-> predid1 "milk" x	)
+	, ( "bananas",	\[x]	-> predid1 "bananas" x	)
+	, ( "eggs",	\[x]	-> predid1 "eggs" x	)
+	, ( "rice",	\[x]	-> predid1 "rice" x	)
+	, ( "fruit_store",	\[x]	-> predid1 "fruit_store" x	)
+
+	, ( "money",	\[x]	-> predid1 "money" x	)
+
+	]
+
+inflections :: [(String, String)]
+inflections = [
+ ( "offer_to_buy", "offered_to_buy" )
+ , ( "went", "go" )
+ , ( "liked", "like" )
+ , ( "bought", "buy" )
+ , ( "want_to_pay", "wanted_to_pay" )
+ , ( "sold", "sell" )
+ , ( "want_to_sell", "wanted_to_sell" )
+ , ( "want_to_buy", "wanted_to_buy" )
+ , ( "want_to_look", "wanted_to_look" )
+ , ( "want_to_have", "wanted_to_have" )
+ , ( "paid", "pay" )
+
+ ]
+
+relations :: [(String, [Entity] -> Bool)]
+relations = [
+	( "useful",	\[x]	-> predid1 "useful" x )
+	, ( "old",	\[x]	-> predid1 "old" x	)
+	, ( "true",	\[x] -> predid1 "true" x	)
+	, ( "false",	\[x] -> predid1 "false" x	)
+	, ( "successful",	\[x] -> predid1 "successful" x	)
+	, ( "unsuccessful",	\[x] -> predid1 "unsuccessful" x	)
+	, ( "go",	\[x,y]	-> predid2 "go" y x	)
+	, ( "like",	\[x,y]	-> predid2 "like" y x	)
+	, ( "finish",	\[x,y,z] -> predid3 "finish" z y x	)
+	, ( "pay",	\args -> case args of
+		[x,y,z,w] -> predid4 "pay" w z y x
+		[x,y,z] -> (forgetful4 . predid4) "pay" z y x
+		[x,y] -> (forgetful3 . forgetful4 . predid4) "pay" y x
+		[x] -> (forgetful2 . forgetful3 . forgetful4 . predid4) "pay" x )
+	, ( "have_to_pay",	\args -> case args of
+		[x,y,z] -> predid3 "have_to_pay" z y x )
+
+	, ( "buy", \args -> case args of 
+		[x,y,z,w] -> predid4 "buy" w z y x
+		[x,y,z] -> (forgetful4 . predid4) "buy" z y x
+		[x,y] -> (forgetful3 .forgetful4 . predid4) "buy" y x
+		[x] -> (forgetful2 . forgetful3 . forgetful4 . predid4) "buy" x )
+	, ( "have_to_buy",	\args -> case args of
+		[x,y,z] -> predid3 "have_to_buy" z y x )
+
+	, ( "wanted_to_pay", \args -> case args of 
+		[x,y,z] -> predid3 "wanted_to_pay" z y x )
+
+	, ( "sell", \args -> case args of 
+		[x,y] -> (forgetful3 . forgetful4 . predid4) "sell" y x
+		[x,y,z] -> (forgetful4 . predid4) "sell" z y x
+		[x,y,z,w] -> predid4 "sell" w z y x )
+	, ( "help", \args -> case args of 
+		[x,y] -> predid2 "help" y x )
+	, ( "wanted_to_help", \args -> case args of 
+		[x,y,z] -> predid3 "wanted_to_help" z y x )
+	, ( "make", \args -> case args of 
+		[x,y] -> (forgetful3 . predid3) "make" y x
+		[x,y,z] -> predid3 "make" z y x )
+	, ( "wanted_to_make", \args -> case args of 
+		[x,y,z,w] -> predid4 "wanted_to_make" w z y x
+		[x,y,z] -> (forgetful4 . predid4) "wanted_to_make" z y x)
+	, ( "wanted_to_buy", \args -> case args of 
+		[x] -> (forgetful2 . forgetful3 . forgetful4 . predid4) "wanted_to_buy" x
+		[x,y] -> (forgetful3 . forgetful4 . predid4) "wanted_to_buy" y x
+		[x,y,z] -> (forgetful4 . predid4) "wanted_to_buy" z y x
+		[x,y,z,w] -> predid4 "wanted_to_buy" w z y x )
+
+	]
+
