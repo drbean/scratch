32c5368b74951c39cbfc6a3c67e15c3bb6f7d43b
drbean <drbean@users.noreply.github.com>
2013-10-25 04:52:32 +0000

    DB::single

--- aca/lib/Aca/Controller/Login.pm
+++ aca/lib/Aca/Controller/Login.pm
@@ -55,7 +55,6 @@ sub index : Path : Args(0) {
 			my @leagues;
 			my $exercise = $c->session->{exercise} || $c->request->query_params
 					->{exercise};
-$DB::single=1;
 			my $genre = $c->model("DB::Exercise")->search( {id => $exercise })
 				->first->genre;
 			$c->session->{genre} = $genre;
