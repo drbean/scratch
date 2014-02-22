196a745a321f1e9c0021ff809daea2077b0cfc88
drbean <drbean@users.noreply.github.com>
2014-02-22 03:37:01 +0000

    rewrote_schema_loader.pk:league,genre.has_one:league

--- aca/lib/Aca/Schema/Result/Leaguegenre.pm
+++ aca/lib/Aca/Schema/Result/Leaguegenre.pm
@@ -1,12 +1,12 @@
 use utf8;
-package Aca::Schema::Result::League;
+package Aca::Schema::Result::Leaguegenre;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
 
 =head1 NAME
 
-Aca::Schema::Result::League
+Aca::Schema::Result::Leaguegenre
 
 =cut
 
@@ -32,86 +32,63 @@ extends 'DBIx::Class::Core';
 
 __PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp");
 
-=head1 TABLE: C<league>
+=head1 TABLE: C<leaguegenre>
 
 =cut
 
-__PACKAGE__->table("league");
+__PACKAGE__->table("leaguegenre");
 
 =head1 ACCESSORS
 
-=head2 id
+=head2 league
 
   data_type: 'text'
   is_nullable: 0
 
-=head2 name
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 field
-
-  data_type: 'text'
-  is_nullable: 1
-
 =head2 genre
 
   data_type: 'text'
-  is_nullable: 1
+  is_nullable: 0
 
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
+  "league",
   { data_type => "text", is_nullable => 0 },
-  "name",
-  { data_type => "text", is_nullable => 1 },
-  "field",
-  { data_type => "text", is_nullable => 1 },
   "genre",
-  { data_type => "text", is_nullable => 1 },
+  { data_type => "text", is_nullable => 0 },
 );
 
 =head1 PRIMARY KEY
 
 =over 4
 
-=item * L</id>
+=item * L</league>
+
+=item * L</genre>
 
 =back
 
 =cut
 
-__PACKAGE__->set_primary_key("id");
+__PACKAGE__->set_primary_key("league", "genre");
 
 =head1 RELATIONS
 
-=head2 members
+=head2 league
 
-Type: has_many
+Type: has_one
 
-Related object: L<Aca::Schema::Result::Member>
+Related object: L<Aca::Schema::Result::League>
 
 =cut
 
-__PACKAGE__->has_many(
-  "members",
-  "Aca::Schema::Result::Member",
-  { "foreign.league" => "self.id" },
+__PACKAGE__->has_one(
+  "league",
+  "Aca::Schema::Result::League",
+  { "foreign.league" => "self.league" },
   { cascade_copy => 0, cascade_delete => 0 },
 );
-
-=head2 players
-
-Type: many_to_many
-
-Composing rels: L</members> -> player
-
-=cut
-
-__PACKAGE__->many_to_many("players", "members", "player");
-
 
 # Created by DBIx::Class::Schema::Loader v0.07033 @ 2013-10-20 12:35:06
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:NnsQ5Y2u98pJRLKkxKZFqA
