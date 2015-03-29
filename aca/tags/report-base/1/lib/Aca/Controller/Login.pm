232858114689ccab52ad1964ffab9cbe7fc64782
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2015-03-29 13:27:34 +0000

    r5489

---
+++ aca/tags/report-base/1/lib/Aca/Controller/Login.pm
@@ -0,0 +1,173 @@
+package Aca::Controller::Login;
+
+# $Id: Login.pm 1485 2013-09-24 04:25:31Z drbean $
+
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+=head1 NAME
+
+dic::Controller::Login - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+Login logic. We used to let "guest"s in without a password, or ID and also redirect to exercise list. Now we redirect to the exercise, if it appears as the one argument.
+
+=cut
+
+sub index : Path : Args(0) {
+	my ( $self, $c ) = @_;
+	my $id	   = $c->request->params->{id}		  || "";
+	my $name	 = $c->request->params->{name}		|| "";
+	my $password = lc $c->request->params->{password} || "";
+	if ( $id && $name && $password ) {
+		my $username = $id;
+		if ( $c->authenticate( { id => $username, password => $password } ) ) {
+			$c->session->{player_id} = $id;
+			my $officialrole = 1;
+			if ( $c->check_user_roles($officialrole) ) {
+				$c->stash->{id}   = $id;
+				$c->stash->{name} = $name;
+				my @leagues = $c->model('dicDB::League')->search({});
+				$c->stash->{leagues} = \@leagues;
+				my $jigsawroles = $c->model('dicDB::Jigsawrole');
+				my $oldrole = $jigsawroles->search( { player => $id } )->next;
+				if ($oldrole) {
+					$c->stash->{oldrole} = $oldrole->role;
+				}
+				$c->stash->{jigsawroles} =
+				  [ $jigsawroles->get_column('role')->func('DISTINCT') ];
+				$c->stash->{template} = 'official.tt2';
+				return;
+			}
+			my @memberships =
+			  $c->model("dicDB::Member")->search( { player => $id } );
+			my @leagues;
+			my $exercise = $c->session->{exercise} || $c->request->query_params
+					->{exercise};
+			my $genre = $c->model("DB::Exercise")->search( {id => $exercise })
+				->first->genre;
+			$c->session->{genre} = $genre;
+			for my $membership (@memberships) {
+				push @leagues, $membership->league if
+					$membership->league->genre->genre eq $genre;
+			}
+			if ( @leagues > 1 ) {
+				$c->stash->{id}	   = $id;
+				$c->stash->{name}	 = $name;
+				$c->stash->{leagues}  = \@leagues;
+				$c->session->{exercise} = $exercise if $exercise;
+				$c->stash(exercise => $exercise);
+				$c->stash->{template} = 'membership.tt2';
+				return;
+			}
+			$c->session->{league} = $leagues[0]->id;
+			if ( defined $c->session->{exercise} ) {
+				my $exercise = $c->session->{exercise};
+				$c->response->redirect(
+					$c->uri_for("/report"), 303 );
+			}
+			else {
+				my $league = $leagues[0]->id;
+				$c->session->{league} = $league;
+				$exercise = $c->forward( 'get_exercise', [ $league ] ) unless $exercise;
+				$c->session->{exercise} = $exercise if $exercise;
+				$c->response->redirect($c->uri_for( "/report"));
+			}
+			return;
+		}
+		else {
+			$c->stash->{error_msg} = "Bad username or password.";
+		}
+	}
+	else {
+		$c->stash->{error_msg} = "You need id, name and password.";
+	}
+	$c->response->header( 'Cache-Control' => 'no-cache' );
+	$c->stash->{template} = 'login.tt2';
+}
+
+=head2 official
+
+Set league official is organizing. Use session player_id to authenticate the participant. Also set jigsaw role the official is taking.
+
+=cut
+
+sub official : Local {
+	my ($self, $c) = @_;
+	my $league = $c->request->params->{league} || "";
+	my $jigsawrole = $c->request->params->{jigsawrole} || "";
+	my $password = lc $c->request->params->{password} || "";
+	my $exercise = $c->request->params->{exercise} || "";
+	my $username = $c->session->{player_id};
+	if ( $c->authenticate( {id =>$username, password=>$password} ) ) {
+		# my $officialrole = "official";
+		my $officialrole = 1;
+		if ( $c->check_user_roles($officialrole) ) {
+			$c->session->{exercise} = $exercise if $exercise;
+			$c->session->{league} = $league;
+			$c->model('dicDB::Jigsawrole')->update_or_create(
+				{ league => $league, player => $username, role => $jigsawrole } )
+					if defined $jigsawrole;
+			$c->response->redirect($c->uri_for("/report"), 303);
+			return;
+		}
+		else {
+			# Set an error message
+			$c->stash->{error_msg} = "Bad official name or password?";
+			$c->stash->{template} = 'login.tt2';
+			return;
+		}
+	}
+	$c->response->header( 'Cache-Control' => 'no-cache' );
+	$c->stash->{error_msg} = "No '$username' username or bad password?";
+	$c->stash->{template} = 'login.tt2';
+}
+
+
+=head2 membership
+
+Set league multi-membership player is participating in.
+
+=cut
+
+sub membership : Local {
+	my ($self, $c) = @_;
+	my $league = $c->request->params->{league} || "";
+	my $password = $c->request->params->{password} || "";
+	my $exercise = $c->request->params->{exercise} || "";
+	$c->session->{league} = $league;
+	$c->session->{exercise} = $exercise if $exercise;
+	if ( $exercise ) {
+		$c->response->redirect(
+			$c->uri_for( "/report" ));
+	}
+	else {
+		$c->response->redirect( $c->uri_for("/exercises/list"), 303 );
+	}
+	return;
+}
+
+
+=head1 AUTHOR
+
+Dr Bean,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ aca/tags/report-base/1/lib/Aca/Controller/Play.pm
@@ -0,0 +1,224 @@
+package Aca::Controller::Play;
+use Moose;
+use namespace::autoclean;
+
+use List::MoreUtils qw/all/;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Bett::Controller::Play - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+use List::MoreUtils qw/any/;
+
+#=head2 index
+#
+#=cut
+#
+#sub index :Path :Args(0) {
+#    my ( $self, $c ) = @_;
+#    $c->stash( config => $c->config );
+#    $c->stash( template => 'exchange.tt2' );
+#}
+
+=cut
+
+=head2 setup
+
+Gameover if all answers defined.
+
+=cut
+
+sub setup :Chained('/') :PathPart('play') :CaptureArgs(1) {
+	my ($self, $c, $mycourse) = @_;
+        my $player = $c->session->{player_id};
+	my $league = $c->session->{league};
+	my $exercise = $c->session->{exercise};
+	my $standing = $c->model("DB::Play")
+		->search({ player => $player,
+		exercise => $exercise,
+		league => $league });
+	my $base = "sport";
+	my $word_bank = $c->model("DB::Word")
+		->search({ exercise => $base });
+	my $play = $c->model("DB::Play")
+		->search({ player => $player,
+		exercise => $base,
+		league => $league });
+	my @word;
+	while ( my $word = $word_bank->next ) {
+		my $head = $word->head;
+		my $my_answer;
+		if ( my $my_word = $play->find({ word => $head }) ) {
+			$my_answer = $my_word->answer;
+		}
+		if ( $my_answer and $my_answer ne $word->answer ) {
+			push @word, $word;
+		}
+	}
+	@word = $c->model("DB::Word")
+		->search({ exercise => $base }) unless @word;
+	#if ( $standing->count >= $word_bank->count ) {
+	#	$c->stash(gameover => 1);
+	#	$c->detach('exchange');
+	#}
+	$c->stash(word => \@word);
+	$c->stash(standing => $standing);
+	$c->stash(course => $mycourse);
+	$c->stash(player => $player);
+	$c->stash(exercise => $exercise);
+	$c->stash(league => $league);
+}
+
+=head2 try
+
+Course, question, answer and Questioner's course and answer. Errors from haskell's Questioner: expectedcourse contains  unknown words and theanswer shows Questioner unhandles it.
+
+=cut
+
+sub try :Chained('setup') :PathPart('') :CaptureArgs(0) {
+	my ( $self, $c ) = @_;
+	my $ex = $c->stash->{exercise};
+	if ( $c->request->params ) {
+		my $course = $c->stash->{course};
+		my $in_play = $c->request->params;
+		delete $in_play->{course};
+		delete $in_play->{shift};
+		delete $in_play->{Submit};
+		$c->stash({ in_play => $in_play });
+		my @heads = keys %$in_play;
+		my $tries = $c->model('DB::Try')->search({
+			league => $c->stash->{league},
+			exercise => $c->stash->{exercise},
+			player => $c->stash->{player},
+			});
+		my $last_try = $tries->get_column('try')->max() + 1;
+		for my $word ( @heads ) {
+			my $answer = $in_play->{$word};
+			$tries->create({
+					try => $last_try,
+					word => $word,
+					answer => $answer
+				}) if $answer;
+		}
+		$c->stash({tries => $tries, try => $last_try});
+	}
+}
+
+=head2 update
+
+TODO standings not checked for dupes in in_play
+
+=cut
+
+sub update :Chained('try') :PathPart('') :CaptureArgs(0) {
+	my ( $self, $c ) = @_;
+	my $player = $c->stash->{ player };
+	my $exercise= $c->stash->{exercise };
+	my $league= $c->stash->{ league };
+	my $standing = $c->stash->{standing};
+	my $tries = $c->stash->{tries};
+	my $last_try = $c->stash->{last_try};
+	my $in_play = $c->stash->{in_play};
+	my $words = $c->stash->{word};
+	my (%dupes, %values, %value_dupes, $error_msg);
+	for ( keys %$in_play ) {
+		my $value = $in_play->{$_};
+		$values{$value}++ if $value;
+	}
+	my @values = values %values;
+	if ( any { $values{$_} > 1 } keys %values ) {
+		my @overvalued = grep { $values{$_} > 1 } keys %values;
+		for my $value ( @overvalued ) {
+			my $value_dupe = $value_dupes{$value};
+			for my $key ( keys %$in_play ) {
+				if ( $in_play->{$key} eq $value ) {
+					$dupes{$key} = $value;
+					push @$value_dupe, $key;
+				}
+			}
+			$value_dupes{$value} = $value_dupe;
+		}
+		for my $value ( keys %value_dupes ) {
+			my $keys = $value_dupes{$value};
+			my $first_word = shift @$keys;
+			#for my $dupe ( @$keys ) {
+			#	$error_msg .= "<br> You gave '$first_word' and '$dupe' the same \
+#translation, '$value'. Choose a different translation for one of them. </br> ";
+			#}
+			#$error_msg .= "<br/>";
+		}
+	}
+	for my $word ( %$in_play ) {
+		my $answer = $in_play->{$word};
+		next unless $answer;
+		my $existing_words = $standing->search({ answer => $answer });
+		while ( my $standing = $existing_words->next ) {
+			my $word_in_standing = $standing->word;
+			$dupes{ $word } = $answer;
+			$dupes{ $word_in_standing } = $answer;
+			push @{ $value_dupes{$answer} }, $word;
+			push @{ $value_dupes{$answer} }, $word_in_standing;
+			# $error_msg .= "<br> Previously, you gave '$word_in_standing' the same \
+# translation as '$word', namely, '$answer'. Choose a different translation for one \
+# of them. </br> ";
+		}
+		#if ( $value_dupes{$answer} ) {
+		#	$existing_words->delete unless $existing_words == 0;
+		#}
+		#else {
+			$standing->update_or_create({ word => $word, answer => $answer,
+			try => $c->stash->{try} });
+		#}
+	}
+	my $progress = $standing->count;
+		$c->stash({ progress => $progress });
+		$c->stash(dupes => \%dupes);
+		$c->stash({error_msg => $error_msg});
+		$c->stash({ word => $words });
+}
+
+=head2 exchange
+
+GAME OVER, or loop back to REPL.
+
+=cut
+
+sub exchange :Chained('update') :PathPart('') :Args(0) {
+	my ( $self, $c ) = @_;
+	if ( $c->stash->{gameover} ) {
+		$c->detach("Report", 'grade');
+	}
+	my $standing = $c->stash->{standing};
+	my $answers;
+	$standing->reset;
+	while ( my $play = $standing->next ) {
+		$answers->{$play->word} = $play->answer if $play->answer;
+	}
+	$c->stash({ answers => $answers });
+	$c->stash->{ template } = 'play.tt2';
+}
+
+=head1 AUTHOR
+
+Dr Bean
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
