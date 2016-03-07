43db4dd38104ecaca98e6403f0938e9ccf917061
drbean <drbean@users.noreply.github.com>
2016-03-07 10:35:53 +0000

    032^042

--- aca/trunk/lib/Aca/Model/dicDB.pm
+++ aca/trunk/lib/Aca/Model/dicDB.pm
@@ -7,7 +7,7 @@ use base 'Catalyst::Model::DBIC::Schema';
 # use Bett;
 
 # my $name = Bett->config->{dic_database};
-my $name = "dic032";
+my $name = "dic042";
 
 __PACKAGE__->config(
     schema_class => 'Aca::dicSchema',
