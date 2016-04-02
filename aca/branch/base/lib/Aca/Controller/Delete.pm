26e875ab53a4c203dff9a39062afdb9b7ecc70de
drbean <drbean@e4cb101e-cafe-4da9-9ea6-c7f86b8dbfb3>
2016-04-02 03:07:51 +0000

    choose^delete,base.reset_for_pre_total_count.return_to_play^user_link_in_delete.tt2

--- aca/branch/base/lib/Aca/Controller/Delete.pm
+++ aca/branch/base/lib/Aca/Controller/Delete.pm
@@ -35,30 +35,6 @@ sub index :Path :Args(0) {
 
 =cut
 
-sub choose :Path :Args(0) {
-	my ($self, $c) = @_;
-	my $id = $c->session->{player_id};
-	my $league   = $c->session->{league};
-	my $exercise = $c->session->{exercise};
-	my $words = $c->model("DB::Word")->search({
-		exercise =>  $exercise});
-	my $base = $c->model("DB::Play")->search({
-		league => $league, exercise => $exercise . "_base", player => $id });
-	my $pre_total = $base->count;
-	my %attempted;
-	while (my $one = $base->next ) {
-		$attempted{$one->word} = $one->answer;
-	}
-	$c->stash->{player}   = $id;
-	$c->stash->{exercise}   = $exercise;
-	$c->stash->{league}   = $league;
-	$c->stash->{limit}   = $c->config->{limit};
-	$c->stash->{pre_total}   = $pre_total;
-	$c->stash->{attempted}   = \%attempted;
-	$c->stash->{words}  = $words;
-	$c->stash->{template}	= "delete.tt2";
-}
-
 sub delete :Path :Args(0) {
 	my ($self, $c) = @_;
 	my $id = $c->session->{player_id};
@@ -69,8 +45,6 @@ sub delete :Path :Args(0) {
 	my $base = $c->model("DB::Play")->search({
 		league => $league, exercise => $exercise . "_base", player => $id });
 	my $limit = $c->config->{limit};
-	my $pre_total = $base->count;
-	$base->reset;
 	$c->stash({limit => $limit});
 	my $removable = $c->request->params;
 	delete $removable->{shift};
@@ -79,6 +53,8 @@ sub delete :Path :Args(0) {
 	for my $word ( keys %$removable )  {
 		$base->search({ word => $word })->delete;
 	}
+	$base->reset;
+	my $pre_total = $base->count;
 	my %revised;
 	while (my $one = $base->next ) {
 		$revised{$one->word} = $one->answer;
@@ -89,12 +65,8 @@ sub delete :Path :Args(0) {
 	$c->stash->{pre_total}   = $pre_total;
 	$c->stash->{attempted}   = \%revised;
 	$c->stash->{words}  = $words;
-	if ( $base->count > $limit ) {
-		$c->stash->{template}	= "delete.tt2";
-	}
-	else {
-		$c->detach("Play", 'setup');
-	}
+	$base->reset;
+	$c->stash->{template}	= "delete.tt2";
 }
 
 =encoding utf8
